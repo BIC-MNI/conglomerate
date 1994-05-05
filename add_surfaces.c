a2a20561e5cce1cd66aee4007f9d4f42afa9a782
david <david>
1994-05-05 09:05:13 +0000

    Initial revision

---
+++ add_surfaces.c
@@ -0,0 +1,104 @@
+#include  <mni.h>
+
+private  Status  add_polygons(
+    polygons_struct   *summed_polygons,
+    Real              weight,
+    polygons_struct   *polygons,
+    BOOLEAN           first_flag )
+{
+    int     i;
+    Point   scaled;
+
+    if( first_flag )
+    {
+        copy_polygons( polygons, summed_polygons );
+        for_less( i, 0, summed_polygons->n_points )
+            fill_Point( summed_polygons->points[i], 0.0, 0.0, 0.0 );
+    }
+    else if( !polygons_are_same_topology( summed_polygons, polygons ) )
+    {
+        print( "Polygons different topology\n" );
+        return( ERROR );
+    }
+
+    for_less( i, 0, summed_polygons->n_points )
+    {
+        SCALE_POINT( scaled, polygons->points[i], weight );
+        ADD_POINTS( summed_polygons->points[i], summed_polygons->points[i],
+                    scaled );
+    }
+
+    return( OK );
+}
+
+int  main(
+    int    argc,
+    char   *argv[] )
+{
+    Status         status;
+    char           *filename, *output_filename;
+    Real           weight, new_weight;
+    int            n_objects, n_polygons;
+    File_formats   format;
+    object_struct  *out_object;
+    object_struct  **object_list;
+    BOOLEAN        done;
+
+    status = OK;
+
+    initialize_argument_processing( argc, argv );
+
+    if( !get_string_argument( "", &output_filename ) )
+    {
+        print( "Need output_file  argument.\n" );
+        return( 1 );
+    }
+
+    done = FALSE;
+    n_polygons = 0;
+    weight = 1.0;
+
+    out_object = create_object( POLYGONS );
+
+    while( !done )
+    {
+        if( get_real_argument( 0.0, &new_weight ) )
+            weight = new_weight;
+        else if( !get_string_argument( "", &filename ) )
+            done = TRUE;
+        else if( input_graphics_file( filename, &format, &n_objects,
+                                      &object_list ) != OK )
+        {
+            print( "Couldn't read %s.\n", filename );
+            return( 1 );
+        }
+        else
+        {
+            if( n_objects >= 1 && object_list[0]->object_type == POLYGONS )
+            {
+                print( "%s: Weighted %g\n", filename, weight );
+                if( add_polygons( get_polygons_ptr(out_object), weight,
+                                  get_polygons_ptr(object_list[0]),
+                                  n_polygons == 0) != OK )
+                {
+                    print( "File %s.\n", filename );
+                    return( 1 );
+                }
+                ++n_polygons;
+            }
+
+            delete_object_list( n_objects, object_list );
+        }
+    }
+
+    compute_polygon_normals( get_polygons_ptr(out_object) );
+
+    if( status == OK )
+        status = output_graphics_file( output_filename, format,
+                                       1, &out_object );
+
+    if( status == OK )
+        print( "Objects output.\n" );
+
+    return( status != OK );
+}
