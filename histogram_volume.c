bcbd4a1dc09975be5abe861aa739eacd25db1f1f
david <david>
1994-08-09 14:35:22 +0000

    *** empty log message ***

--- histogram_volume.c
+++ histogram_volume.c
@@ -1,14 +1,28 @@
 #include  <module.h>
 
-#define  DEFAULT_N_INTERVALS  100
+#define  DEFAULT_N_INTERVALS  255
+
+#define  WINDOW_WIDTH 0.05
+
+private  BOOLEAN  is_maximum(
+    int     counts[],
+    int     i,
+    int     n,
+    int     width );
+private  BOOLEAN  is_minimum(
+    int     counts[],
+    int     i,
+    int     n,
+    int     width );
 
 int  main(
     int   argc,
     char  *argv[] )
 {
     int                  x, y, z, sizes[N_DIMENSIONS], x_size, y_size;
-    Real                 value;
-    Real                 min_value, max_value;
+    int                  n, i, *counts, window_width;
+    Real                 value, width_ratio;
+    Real                 min_value, max_value, scale, trans;
     char                 *input_volume_filename, *output_filename;
     lines_struct         *lines;
     object_struct        *object;
@@ -26,6 +40,7 @@ int  main(
 
     (void) get_int_argument( DEFAULT_N_INTERVALS, &x_size );
     (void) get_int_argument( x_size, &y_size );
+    (void) get_real_argument( WINDOW_WIDTH, &width_ratio );
 
     if( input_volume( input_volume_filename, 3, XYZ_dimension_names,
                       NC_UNSPECIFIED, FALSE, 0.0, 0.0,
@@ -35,7 +50,9 @@ int  main(
     get_volume_sizes( volume, sizes );
     get_volume_real_range( volume, &min_value, &max_value );
 
-    initialize_histogram( &histogram, (max_value - min_value) / 1000.0, 0.0 );
+    initialize_histogram( &histogram,
+                          (max_value - min_value) / (Real) (x_size-1),
+                          min_value );
 
     for_less( x, 0, sizes[X] )
     {
@@ -57,5 +74,63 @@ int  main(
 
     (void) output_graphics_file( output_filename, ASCII_FORMAT, 1, &object );
 
+    window_width = ROUND( x_size * width_ratio );
+    if( window_width < 1 )
+        window_width = 1;
+
+    n = get_histogram_counts( &histogram, &counts, &scale, &trans );
+
+    for_less( i, 0, n )
+    {
+        if( is_minimum( counts, i, n, window_width ) )
+            print( "Minimum at %g\n", scale * (Real) i + trans );
+        else if( is_maximum( counts, i, n, window_width ) )
+            print( "   Maximum at %g\n", scale * (Real) i + trans );
+    }
+
     return( 0 );
+}
+
+private  BOOLEAN  is_minimum(
+    int     counts[],
+    int     i,
+    int     n,
+    int     width )
+{
+    int   d, m1, m2;
+
+    if( i != 0 && counts[i-1] < counts[i] ||
+        i < n-1 && counts[i+1] < counts[i] )
+        return( FALSE );
+
+    m1 = MAX( 0, i - width );
+    m2 = MIN( n-1, i + width );
+
+    for_inclusive( d, m1, m2 )
+        if( counts[d] < counts[i] )
+            return( FALSE );
+
+    return( TRUE );
+}
+
+private  BOOLEAN  is_maximum(
+    int     counts[],
+    int     i,
+    int     n,
+    int     width )
+{
+    int   d, m1, m2;
+
+    if( i != 0 && counts[i-1] > counts[i] ||
+        i < n-1 && counts[i+1] > counts[i] )
+        return( FALSE );
+
+    m1 = MAX( 0, i - width );
+    m2 = MIN( n-1, i + width );
+
+    for_inclusive( d, m1, m2 )
+        if( counts[d] > counts[i] )
+            return( FALSE );
+
+    return( TRUE );
 }
