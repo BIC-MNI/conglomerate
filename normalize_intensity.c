4233132ba8413ceb89841784b911967c0120d3d4
david <david>
1995-01-26 01:47:03 +0000

    vvi

--- normalize_intensity.c
+++ normalize_intensity.c
@@ -6,6 +6,10 @@ private  Real  evaluate_error(
     Volume   volume,
     Real     max_diff );
 
+private  void  normalize_intensities(
+    Volume   volume,
+    Real     max_diff );
+
 int  main(
     int   argc,
     char  *argv[] )
@@ -34,7 +38,6 @@ int  main(
 
     print( "Before: %g\n", before );
 
-/*
     normalize_intensities( volume, max_diff );
 
     after = evaluate_error( volume, max_diff );
@@ -46,7 +49,6 @@ int  main(
                                    "flip_volume", (minc_output_options *) NULL);
 
     delete_volume( volume );
-*/
 
     return( 0 );
 }
@@ -55,13 +57,14 @@ private  Real  evaluate_error(
     Volume   volume,
     Real     max_diff )
 {
-    int   tx, ty, tz, dx, dy, dz;
+    int   tx, ty, tz, dx, dy, dz, n_voxels;
     int   x, y, z, sizes[N_DIMENSIONS];
     Real  error, value1, value2, diff;
 
     get_volume_sizes( volume, sizes );
 
     error = 0.0;
+    n_voxels = 0;
 
     for_less( x, 0, sizes[0] )
     {
@@ -84,9 +87,9 @@ private  Real  evaluate_error(
 
                 GET_VALUE_3D( value1, volume, x, y, z );
 
-                for_less( dx, 0, tx )
-                for_less( dy, 0, ty )
-                for_less( dz, 0, tz )
+                for_inclusive( dx, 0, tx )
+                for_inclusive( dy, 0, ty )
+                for_inclusive( dz, 0, tz )
                 {
                     if( dx == 0 && dy == 0 && dz == 0 )
                         continue;
@@ -98,11 +101,208 @@ private  Real  evaluate_error(
                         diff = -diff;
 
                     if( diff <= max_diff )
-                        error += (value1 - value2) * (value1 - value2);
+                    {
+                        error += diff * diff;
+                        ++n_voxels;
+                    }
                 }
             }
         }
     }
 
+    print( "N voxels: %d\n", n_voxels );
+
     return( error );
+}
+
+private  void  normalize_intensities(
+    Volume   volume,
+    Real     max_diff )
+{
+    int   i, j, tx, ty, tz, dx, dy, dz;
+    int   x, y, z, sizes[N_DIMENSIONS];
+    Real  scale, trans, voxel, sum;
+    Real  answer[6], cx, cy, cz;
+    Real  x1, y1, z1, x2, y2, z2;
+    float d;
+    Real  value1, value2, diff;
+    int   ind[7];
+    float **coefs, **inv, constants[7], p[8], col[7], **save_coefs;
+
+    get_volume_sizes( volume, sizes );
+
+    cx = (Real) (sizes[0]-1) / 2.0;
+    cy = (Real) (sizes[1]-1) / 2.0;
+    cz = (Real) (sizes[2]-1) / 2.0;
+
+    ALLOC2D( coefs, 7, 7 );
+    ALLOC2D( inv, 7, 7 );
+
+    for_less( i, 1, 7 )
+    {
+        for_less( j, i, 7 )
+        {
+            coefs[i][j] = 0.0;
+        }
+        constants[i] = 0.0;
+    }
+
+    for_less( x, 0, sizes[0] )
+    {
+        if( x < sizes[0]-1 )
+            tx = 1;
+        else
+            tx = 0;
+        x1 = x - cx;
+        for_less( y, 0, sizes[1] )
+        {
+            if( y < sizes[1]-1 )
+                ty = 1;
+            else
+                ty = 0;
+            y1 = y - cy;
+            for_less( z, 0, sizes[2] )
+            {
+                if( z < sizes[2]-1 )
+                    tz = 1;
+                else
+                    tz = 0;
+                z1 = z - cz;
+
+                GET_VALUE_3D( value1, volume, x, y, z );
+
+                for_inclusive( dx, 0, tx )
+                for_inclusive( dy, 0, ty )
+                for_inclusive( dz, 0, tz )
+                {
+                    if( dx == 0 && dy == 0 && dz == 0 )
+                        continue;
+
+                    GET_VALUE_3D( value2, volume, x + dx, y + dy, z + dz );
+
+                    diff = value1 - value2;
+                    if( diff < 0.0 )
+                        diff = -diff;
+
+                    if( diff <= max_diff )
+                    {
+                        x2 = x + dx - cx;
+                        y2 = y + dy - cy;
+                        z2 = z + dz - cz;
+
+                        p[1] = ((Real) x1 * value1 - (Real) x2 * value2);
+                        p[2] = ((Real) y1 * value1 - (Real) y2 * value2);
+                        p[3] = ((Real) z1 * value1 - (Real) z2 * value2);
+
+                        p[4] = (Real) x1 - (Real) x2;
+                        p[5] = (Real) y1 - (Real) y2;
+                        p[6] = (Real) z1 - (Real) z2;
+
+                        p[7] = value1 - value2;
+
+                        for_less( i, 1, 7 )
+                        {
+                            for_less( j, i, 7 )
+                            {
+                                coefs[i][j] += p[i] * p[j];
+                            }
+                            constants[i] += p[i] * p[7];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for_less( i, 1, 6 )
+    {
+        for_less( j, i+1, 7 )
+        {
+            coefs[j][i] = coefs[i][j];
+        }
+    }
+
+    ALLOC2D( save_coefs, 7, 7 );
+    for_less( i, 1, 7 )
+        for_less( j, 1, 7 )
+            save_coefs[i][j] = coefs[i][j];
+
+    ludcmp( coefs, 6, ind, &d );
+
+    if( d != 0.0 )
+    {
+        for_inclusive( j, 1, 6 )
+        {
+            for_inclusive( i, 1, 6 )
+                col[i] = 0.0;
+            col[j] = 1.0;
+            lubksb( coefs, 6, ind, col );
+            for_inclusive( i, 1, 6 )
+                inv[i][j] = col[i];
+        }
+
+        for_inclusive( i, 1, 6 )
+        {
+            for_inclusive( j, 1, 6 )
+            {
+                int  k;
+
+                sum = 0.0;
+                for_inclusive( k, 1, 6 )
+                    sum += save_coefs[i][k] * inv[k][j];
+                print( " %.1f", sum );
+            }
+            print( "\n" );
+        }
+
+        for_less( i, 1, 7 )
+        {
+            sum = 0.0;
+            for_less( j, 1, 7 )
+                sum += inv[i][j] * (-constants[j]);
+            answer[i-1] = sum;
+        }
+
+        for_less( i, 1, 7 )
+        {
+            sum = 0.0;
+            for_less( j, 1, 7 )
+                sum += save_coefs[i][j] * answer[j-1];
+            sum += constants[i];
+
+            if( sum > 1.0e-2 || sum < -1.0e-2 )
+            {
+                print( "%g\n", sum );
+/*                handle_internal_error( "Dangl\n" ); */
+            }
+        }
+
+        for_less( i, 0, 6 )
+            print( "Coef[%d] = %g\n", i, answer[i] );
+    }
+    else
+    {
+        print( "No inverse.\n" );
+        FREE2D( coefs );
+        FREE2D( save_coefs );
+        FREE2D( inv );
+        return;
+    }
+
+    FREE2D( coefs );
+    FREE2D( save_coefs );
+    FREE2D( inv );
+
+    for_less( x, 0, sizes[0] )
+    for_less( y, 0, sizes[1] )
+    for_less( z, 0, sizes[2] )
+    {
+        GET_VALUE_3D( value1, volume, x, y, z );
+        scale = 1.0 +
+                answer[0] * (x-cx) + answer[1] * (y-cy) + answer[2] * (z-cz);
+        trans = answer[3] * (x-cx) + answer[4] * (y-cy) + answer[5] * (z-cz);
+        value1 = scale * value1 + trans;
+        voxel = CONVERT_VALUE_TO_VOXEL( volume, value1 );
+        SET_VOXEL_3D( volume, x, y, z, voxel );
+    }
 }
