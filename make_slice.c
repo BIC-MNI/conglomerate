1ab483187d6c50ba113e698f6c853c0e36682bc2
david <david>
1995-07-06 14:26:39 +0000

    *** empty log message ***

--- make_slice.c
+++ make_slice.c
@@ -1,4 +1,5 @@
-#include  <module.h>
+#include  <internal_volume_io.h>
+#include  <bicpl.h>
 
 int  main(
     int   argc,
@@ -9,29 +10,25 @@ int  main(
     Volume               volume;
     volume_input_struct  volume_input;
     int                  axis, x_tess, y_tess;
+    BOOLEAN              voxel_slice;
     object_struct        *object;
     Real                 world_pos, x_min, x_max, y_min, y_max;
     Real                 world[N_DIMENSIONS], voxel[MAX_DIMENSIONS];
+    Real                 xw, yw, zw, xvw, yvw, zvw;
+    Point                origin;
+    Vector               normal;
 
     initialize_argument_processing( argc, argv );
 
     if( !get_string_argument( "", &input_volume_filename ) ||
-        !get_string_argument( "", &output_filename ) ||
-        !get_string_argument( "", &axis_name ) ||
-        !get_real_argument( 0.0, &world_pos ) )
+        !get_string_argument( "", &output_filename ) )
     {
-        print( "Usage: %s  volume_file  output_file  x|y|z  world_pos\n",
+        print( "Usage: %s  volume_file  output_file  x|y|z  world_pos [xt] [yt]\n",
                argv[0] );
         return( 1 );
     }
 
-    (void) get_int_argument( 0, &x_tess );
-    (void) get_int_argument( 0, &y_tess );
-
-    (void) get_real_argument( 0.0, &x_min );
-    (void) get_real_argument( 0.0, &x_max );
-    (void) get_real_argument( 0.0, &y_min );
-    (void) get_real_argument( 0.0, &y_max );
+    (void) get_string_argument( "", &axis_name );
 
     if( strcmp( axis_name, "x" ) == 0 )
         axis = X;
@@ -40,7 +37,35 @@ int  main(
     else if( strcmp( axis_name, "z" ) == 0 )
         axis = Z;
     else
-        axis = X;
+        axis = -1;
+
+    if( axis >= 0 )
+    {
+        voxel_slice = TRUE;
+
+        (void) get_real_argument( 0.0, &world_pos );
+
+        (void) get_int_argument( 0, &x_tess );
+        (void) get_int_argument( 0, &y_tess );
+
+        (void) get_real_argument( 0.0, &x_min );
+        (void) get_real_argument( 0.0, &x_max );
+        (void) get_real_argument( 0.0, &y_min );
+        (void) get_real_argument( 0.0, &y_max );
+    }
+    else
+    {
+        voxel_slice = FALSE;
+        (void) sscanf( axis_name, "%lf", &xw );
+        (void) get_real_argument( 0.0, &yw );
+        (void) get_real_argument( 0.0, &zw );
+        (void) get_real_argument( 0.0, &xvw );
+        (void) get_real_argument( 0.0, &yvw );
+        (void) get_real_argument( 0.0, &zvw );
+
+        fill_Point( origin, xw, yw, zw );
+        fill_Vector( normal, xvw, yvw, zvw );
+    }
 
     if( start_volume_input( input_volume_filename, 3, XYZ_dimension_names,
                             NC_UNSPECIFIED, FALSE, 0.0, 0.0,
@@ -48,17 +73,25 @@ int  main(
                             &volume_input ) != OK )
         return( 1 );
 
-    world[X] = 0.0;
-    world[Y] = 0.0;
-    world[Z] = 0.0;
-    world[axis] = world_pos;
+    if( voxel_slice )
+    {
+        world[X] = 0.0;
+        world[Y] = 0.0;
+        world[Z] = 0.0;
+        world[axis] = world_pos;
 
-    convert_world_to_voxel( volume, world[X], world[Y], world[Z], voxel );
+        convert_world_to_voxel( volume, world[X], world[Y], world[Z], voxel );
 
-    object = create_object( QUADMESH );
-    create_slice_quadmesh( volume, axis, voxel[axis], x_tess, y_tess,
-                           x_min, x_max, y_min, y_max,
-                           get_quadmesh_ptr(object) );
+        object = create_object( QUADMESH );
+        create_slice_quadmesh( volume, axis, voxel[axis], x_tess, y_tess,
+                               x_min, x_max, y_min, y_max,
+                               get_quadmesh_ptr(object) );
+    }
+    else
+    {
+        object = create_object( POLYGONS );
+        create_slice_3d( volume, &origin, &normal, get_polygons_ptr(object) );
+    }
 
     if( output_graphics_file( output_filename, BINARY_FORMAT, 1, &object )
         != OK )
