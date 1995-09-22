7233e79d2efd937d7288114a369967054b6495cc
david <david>
1995-09-22 13:50:06 +0000

    check_in_all

--- dilate_volume.c
+++ dilate_volume.c
@@ -6,11 +6,12 @@ private  void  usage(
 {
     char  usage_str[] = "\n\
 Usage: dilate_volume input.mnc output.mnc  dilation_value\n\
-            [6|26]  [n_dilations]\n\
+            [6|26]  [n_dilations]  [mask.mnc min_mask max_mask]\n\
 \n\
      Dilates all regions of value dilation_value, by n_dilations of 3X3X3,\n\
      (1 dilation by default).  You can specify 6 or 26 neighbours, default\n\
-     being 26.\n\n";
+     being 26.  If the mask volume and range is specified, then only voxels\n\
+     in the specified mask range will be dilated.\n\n";
 
     print_error( usage_str, executable );
 }
@@ -19,24 +20,36 @@ int  main(
     int   argc,
     char  *argv[] )
 {
-    char                 *input_filename, *output_filename;
-    Volume               volume;
-    int                  i, n_dilations, n_neighs, value_to_dilate;
+    char                 *input_filename, *output_filename, *mask_filename;
+    Real                 min_mask, max_mask, value_to_dilate;
+    BOOLEAN              mask_volume_present;
+    Volume               volume, mask_volume;
+    int                  i, n_dilations, n_neighs;
     Neighbour_types      connectivity;
 
     initialize_argument_processing( argc, argv );
 
     if( !get_string_argument( "", &input_filename ) ||
         !get_string_argument( "", &output_filename ) ||
-        !get_int_argument( 0, &value_to_dilate ) )
+        !get_real_argument( 0.0, &value_to_dilate ) )
     {
-        print( "%s input.mnc output.mnc  dilation_value\n", argv[0] );
-        print( "      [6|26]  [n_dilations]\n" );
+        usage( argv[0] );
         return( 1 );
     }
 
     (void) get_int_argument( 8, &n_neighs );
     (void) get_int_argument( 1, &n_dilations );
+
+    mask_volume_present = get_string_argument( "", &mask_filename );
+
+    if( mask_volume_present &&
+        !get_real_argument( 0.0, &min_mask ) ||
+        !get_real_argument( 0.0, &mask_mask ) )
+    {
+        usage( argv[0] );
+        return( 1 );
+    }
+
 
     switch( n_neighs )
     {
@@ -50,13 +63,27 @@ int  main(
                       (minc_input_options *) NULL ) != OK )
         return( 1 );
 
+    if( mask_volume_present )
+    {
+        if( input_volume( mask_volume, 3, XYZ_dimension_names,
+                          NC_UNSPECIFIED, FALSE, 0.0, 0.0, TRUE, &mask_volume,
+                          (minc_input_options *) NULL ) != OK )
+            return( 1 );
+    }
+    else
+    {
+        mask_volume = NULL;
+        min_mask = -1.0;
+        max_mask = 0.0;
+    }
+
     for_less( i, 0, n_dilations )
     {
-        dilate_labeled_voxels_3d( NULL, volume,
+        dilate_labeled_voxels_3d( mask_volume, volume,
                                   0, -1,
-                                  0.0, -1.0,
+                                  min_mask, max_mask,
                                   0, -1, 0.0, -1.0,
-                                  value_to_dilate, connectivity );
+                                  (int) value_to_dilate, connectivity );
     }
 
     (void) output_modified_volume( output_filename, NC_UNSPECIFIED, FALSE,
