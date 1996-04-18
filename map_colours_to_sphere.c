ac5d931ae4cf73564a4d38a1fdfa90f47ce50546
david <david>
1996-04-18 12:19:01 +0000

    *** empty log message ***

--- map_colours_to_sphere.c
+++ map_colours_to_sphere.c
@@ -4,6 +4,7 @@
 private  void  map_colours_to_sphere_topology(
     Volume            volume,
     polygons_struct   *polygons,
+    BOOLEAN           origin_specified,
     Point             *origin,
     Point             *equator );
 
@@ -19,6 +20,7 @@ int  main(
     polygons_struct      *polygons;
     Real                 origin_x, origin_y, origin_z;
     Real                 equator_x, equator_y, equator_z;
+    BOOLEAN              origin_specified;
     Point                origin, equator;
     STRING               dim_names[4] = { MIxspace,
                                           MIyspace,
@@ -41,7 +43,7 @@ int  main(
     (void) get_real_argument( 0.0, &origin_z );
     (void) get_real_argument( 0.0, &equator_x );
     (void) get_real_argument( 1.0, &equator_y );
-    (void) get_real_argument( 0.0, &equator_z );
+    origin_specified = get_real_argument( 0.0, &equator_z );
 
     fill_Point( origin, origin_x, origin_y, origin_z );
     fill_Point( equator, equator_x, equator_y, equator_z );
@@ -77,7 +79,8 @@ int  main(
     REALLOC( polygons->colours, polygons->n_points );
     polygons->colour_flag = PER_VERTEX_COLOURS;
 
-    map_colours_to_sphere_topology( volume, polygons, &origin, &equator );
+    map_colours_to_sphere_topology( volume, polygons, origin_specified,
+                                    &origin, &equator );
 
     (void) output_graphics_file( output_filename, format, n_objects,
                                  object_list );
@@ -86,28 +89,32 @@ int  main(
 }
 
 private  void  convert_sphere_point_to_2d(
-    Point   *point,
-    Real    *u,
-    Real    *v )
+    Point    *origin,
+    Point    *equator_point,
+    Point    *point,
+    Real     *u,
+    Real     *v )
 {
-    Real   angle_up, angle_around;
-    Point  up_dir, hor_dir, vert_dir;
+    Real    angle_up, angle_around, cos_angle_up;
+    Vector  up_dir, hor_dir, vert_dir, non_hor_dir;
 
-    fill_Point( hor_dir, 1.0, 0.0, 0.0 );
-    fill_Point( vert_dir, 0.0, 1.0, 0.0 );
-    fill_Point( up_dir, 0.0, 0.0, 1.0 );
+    CONVERT_POINT_TO_VECTOR( hor_dir, *origin );
+    CONVERT_POINT_TO_VECTOR( non_hor_dir, *equator_point );
+    CROSS_VECTORS( up_dir, hor_dir, non_hor_dir );
+    CROSS_VECTORS( vert_dir, up_dir, hor_dir );
 
-    angle_up = compute_clockwise_rotation( DOT_POINT_VECTOR(*point,up_dir),
-                                           DOT_POINT_VECTOR(*point,vert_dir) );
+    NORMALIZE_VECTOR( hor_dir, hor_dir );
+    NORMALIZE_VECTOR( vert_dir, vert_dir );
+    NORMALIZE_VECTOR( up_dir, up_dir );
 
-    if( angle_up > PI )
-        angle_up = 2 * PI - angle_up;
+    cos_angle_up = DOT_POINT_VECTOR( *point, up_dir );
+    angle_up = acos( cos_angle_up );
 
     *v = 1.0 - angle_up / PI;
 
     angle_around = compute_clockwise_rotation(
                          DOT_POINT_VECTOR(*point,hor_dir),
-                         DOT_POINT_VECTOR(*point,vert_dir) );
+                         -DOT_POINT_VECTOR(*point,vert_dir) );
 
     *u = angle_around / (2.0*PI);
 }
@@ -127,13 +134,14 @@ private  void  convert_uv_to_volume_position(
 private  void  map_colours_to_sphere_topology(
     Volume            volume,
     polygons_struct   *polygons,
+    BOOLEAN           origin_specified,
     Point             *origin,
     Point             *equator )
 {
     int              point_index, sizes[MAX_DIMENSIONS];
     Real             u, v, voxel[MAX_DIMENSIONS], value[3];
     Real             min_value, max_value, r, g, b;
-    Point            centre;
+    Point            centre, unit_origin, unit_equator;
     polygons_struct  unit_sphere;
     BOOLEAN          interpolating[4] = { TRUE, TRUE, TRUE, FALSE };
 
@@ -145,9 +153,31 @@ private  void  map_colours_to_sphere_topology(
     create_tetrahedral_sphere( &centre, 1.0, 1.0, 1.0,
                                polygons->n_items, &unit_sphere );
 
+    if( origin_specified )
+    {
+        map_point_to_unit_sphere( polygons, origin, &unit_sphere,
+                                  &unit_origin );
+        map_point_to_unit_sphere( polygons, equator, &unit_sphere,
+                                  &unit_equator );
+        print( "Used unit origin: %g %g %g\n",
+               Point_x(unit_origin),
+               Point_y(unit_origin),
+               Point_z(unit_origin) );
+        print( "Used unit equator: %g %g %g\n",
+               Point_x(unit_equator),
+               Point_y(unit_equator),
+               Point_z(unit_equator) );
+    }
+    else
+    {
+        fill_Point( unit_origin, 1.0, 0.0, 0.0 );
+        fill_Point( unit_equator, 0.0, 1.0, 0.0 );
+    }
+
     for_less( point_index, 0, polygons->n_points )
     {
-        convert_sphere_point_to_2d( &unit_sphere.points[point_index], &u, &v );
+        convert_sphere_point_to_2d( &unit_origin, &unit_equator,
+                                    &unit_sphere.points[point_index], &u, &v );
 
         convert_uv_to_volume_position( sizes, u, v, voxel );
 
