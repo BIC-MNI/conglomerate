f05c6e95ac67fd19185cfe14d3ec04aefd6139d1
david <david>
1996-07-04 15:00:30 +0000

    *** empty log message ***

--- blur_surface.c
+++ blur_surface.c
@@ -9,39 +9,45 @@ private  void  generate_neighbours(
 private  void  gaussian_blur_points(
     int               n_polygon_points,
     Point             points[],
+    Real              values[],
     int               n_neighbours[],
     int               *neighbours[],
     int               point_index,
     Real              fwhm,
     Real              dist,
-    Point             *smooth_point );
+    Point             *smooth_point,
+    Real              *value );
 
 int  main(
     int    argc,
     char   *argv[] )
 {
-    STRING           input_filename, output_filename;
-    int              i, n_objects;
+    STRING           input_filename, output_filename, values_filename;
+    int              n_objects, p;
     int              *n_neighbours, **neighbours;
+    FILE             *file;
     File_formats     format;
     object_struct    **object_list;
     polygons_struct  *polygons;
-    Point            *smooth_points;
-    Real             fwhm, distance;
+    Point            *smooth_points, point;
+    Real             fwhm, distance_ratio, *values, value, *smooth_values;
+    BOOLEAN          values_present;
     progress_struct  progress;
 
     initialize_argument_processing( argc, argv );
 
     if( !get_string_argument( NULL, &input_filename ) ||
+        !get_string_argument( NULL, &output_filename ) ||
         !get_real_argument( 0.0, &fwhm ) ||
-        !get_string_argument( NULL, &output_filename ) )
+        !get_real_argument( 3.0, &distance_ratio ) )
     {
         print_error(
-          "Usage: %s  input.obj  fwhm  out.obj\n", argv[0] );
+          "Usage: %s  input.obj  output.obj fwhm  dist_ratio [values]\n",
+                      argv[0] );
         return( 1 );
     }
 
-    (void) get_real_argument( 3.0, &distance );
+    values_present = get_string_argument( NULL, &values_filename );
 
     if( input_graphics_file( input_filename, &format, &n_objects,
                              &object_list ) != OK || n_objects != 1 ||
@@ -53,6 +59,30 @@ int  main(
 
     polygons = get_polygons_ptr( object_list[0] );
 
+    if( values_present )
+    {
+        ALLOC( values, polygons->n_points );
+        ALLOC( smooth_values, polygons->n_points );
+
+        if( open_file( values_filename, READ_FILE, ASCII_FORMAT, &file ) != OK )
+            return( 1 );
+
+        for_less( p, 0, polygons->n_points )
+        {
+            if( input_real( file, &values[p] ) != OK )
+                return( 1 );
+        }
+
+        (void) close_file( file );
+
+        smooth_points = NULL;
+    }
+    else
+    {
+        ALLOC( smooth_points, polygons->n_points );
+        values = NULL;
+    }
+
     check_polygons_neighbours_computed( polygons );
 
     ALLOC( n_neighbours, polygons->n_points );
@@ -60,28 +90,48 @@ int  main(
 
     generate_neighbours( polygons, n_neighbours, neighbours );
 
-    ALLOC( smooth_points, polygons->n_points );
-
     initialize_progress_report( &progress, FALSE, polygons->n_points,
                                 "Blurring" );
 
-    for_less( i, 0, polygons->n_points )
+    for_less( p, 0, polygons->n_points )
     {
-        gaussian_blur_points( polygons->n_points, polygons->points,
+        gaussian_blur_points( polygons->n_points, polygons->points, values,
                               n_neighbours, neighbours,
-                              i, fwhm, distance, &smooth_points[i] );
+                              p, fwhm, distance_ratio, &point, &value );
 
-        update_progress_report( &progress, i+1 );
+        if( values_present )
+            smooth_values[p] = value;
+        else
+            smooth_points[p] = point;
+
+        update_progress_report( &progress, p+1 );
     }
 
     terminate_progress_report( &progress );
 
-    FREE( polygons->points );
-    polygons->points = smooth_points;
+    if( values_present )
+    {
+        if( open_file( output_filename, WRITE_FILE, ASCII_FORMAT, &file ) != OK)
+            return( 1 );
 
-    compute_polygon_normals( polygons );
+        for_less( p, 0, polygons->n_points )
+        {
+            if( output_real( file, smooth_values[p] ) != OK ||
+                output_newline( file ) != OK )
+                return( 1 );
+        }
 
-    output_graphics_file( output_filename, format, 1, object_list );
+        (void) close_file( file );
+    }
+    else
+    {
+        FREE( polygons->points );
+        polygons->points = smooth_points;
+
+        compute_polygon_normals( polygons );
+
+        output_graphics_file( output_filename, format, 1, object_list );
+    }
 
     return( 0 );
 }
@@ -203,12 +253,14 @@ int  get_points_within_dist(
 private  void  gaussian_blur_points(
     int               n_polygon_points,
     Point             polygon_points[],
+    Real              values[],
     int               n_neighbours[],
     int               *neighbours[],
     int               point_index,
     Real              fwhm,
     Real              dist,
-    Point             *smooth_point )
+    Point             *smooth_point,
+    Real              *value )
 {
     Real   sum[3], weight, sum_weight, point_dist, e_const;
     int    i, c, n_points, *points, neigh;
@@ -233,12 +285,25 @@ private  void  gaussian_blur_points(
 
         weight = evaluate_gaussian( point_dist, e_const );
 
-        for_less( c, 0, N_DIMENSIONS )
-            sum[c] += weight * (Real) Point_coord(polygon_points[neigh],c);
+        if( values != NULL )
+            sum[0] += weight * values[neigh];
+        else
+        {
+            for_less( c, 0, N_DIMENSIONS )
+                sum[c] += weight * (Real) Point_coord(polygon_points[neigh],c);
+        }
 
         sum_weight += weight;
     }
 
-    for_less( c, 0, N_DIMENSIONS )
-        Point_coord(*smooth_point,c) = (Point_coord_type) (sum[c] / sum_weight);
+    if( values != NULL )
+    {
+        *value = sum[0] / sum_weight;
+    }
+    else
+    {
+        for_less( c, 0, N_DIMENSIONS )
+            Point_coord(*smooth_point,c) = (Point_coord_type) (sum[c] /
+                                                               sum_weight);
+    }
 }
