01549a6fbef33895016fc51c1a7445a5ea67f20e
david <david>
1996-10-08 15:26:49 +0000

    Initial revision

---
+++ split_polygons.c
@@ -0,0 +1,291 @@
+#include <internal_volume_io.h>
+#include <bicpl.h>
+
+private  int   split_polygons(
+    polygons_struct    *polygons,
+    polygons_struct    *out1,
+    polygons_struct    *out2,
+    int                translation1[],
+    int                translation2[] );
+
+private  void  usage(
+    STRING   executable )
+{
+    STRING  usage_str = "\n\
+Usage: %s  input.obj  output1.obj output2.obj [translation.out]\n\
+\n\
+     Splits polygons into two separate halves.\n\n";
+
+    print_error( usage_str, executable );
+}
+
+int  main(
+    int    argc,
+    char   *argv[] )
+{
+    STRING           input_filename, output1_filename, output2_filename;
+    STRING           translation_filename;
+    int              n_objects, *translation1, *translation2;
+    int              n_in_half;
+    File_formats     format;
+    object_struct    **object_list, *object1, *object2;
+    polygons_struct  *polygons;
+
+    initialize_argument_processing( argc, argv );
+
+    if( !get_string_argument( NULL, &input_filename ) ||
+        !get_string_argument( NULL, &output1_filename ) ||
+        !get_string_argument( NULL, &output2_filename ) )
+    {
+        usage( argv[0] );
+        return( 1 );
+    }
+
+    (void) get_string_argument( NULL, &translation_filename );
+
+    if( input_graphics_file( input_filename, &format, &n_objects,
+                             &object_list ) != OK || n_objects < 1 ||
+        get_object_type( object_list[0] ) != POLYGONS )
+    {
+        print_error( "File must have a polygons structure.\n" );
+        return( 1 );
+    }
+
+    polygons = get_polygons_ptr( object_list[0] );
+    check_polygons_neighbours_computed( polygons );
+
+    object1 = create_object( POLYGONS );
+    initialize_polygons( get_polygons_ptr(object1), WHITE, NULL );
+    object2 = create_object( POLYGONS );
+    initialize_polygons( get_polygons_ptr(object2), WHITE, NULL );
+
+    ALLOC( translation1, polygons->n_points );
+    ALLOC( translation2, polygons->n_points );
+
+    n_in_half = split_polygons( polygons, get_polygons_ptr(object1),
+                                get_polygons_ptr(object2),
+                                translation1, translation2 );
+
+    print( "Split into %d and %d polygons.\n", n_in_half,
+           polygons->n_items - n_in_half );
+
+    (void) output_graphics_file( output1_filename, format,
+                                 1, &object1 );
+    (void) output_graphics_file( output2_filename, format,
+                                 1, &object2 );
+
+    return( 0 );
+}
+
+private  BOOLEAN   can_include(
+    polygons_struct  *polygons,
+    int              target_poly,
+    Smallest_int     included[],
+    Smallest_int     connected[] )
+{
+    int                n_neighbours, neighbours[10000];
+    int                poly, neigh_poly, size, edge, p, n, n_connected;
+    QUEUE_STRUCT(int)  queue;
+
+    size = GET_OBJECT_SIZE( *polygons, target_poly );
+
+    n_neighbours = 0;
+    for_less( edge, 0, size )
+    {
+        neigh_poly = polygons->neighbours[
+                         POINT_INDEX(polygons->end_indices,target_poly,edge)];
+        if( neigh_poly >= 0 && !included[neigh_poly] )
+        {
+            neighbours[n_neighbours] = neigh_poly;
+            ++n_neighbours;
+        }
+    }
+
+    if( n_neighbours < 0 )
+        return( TRUE );
+
+    for_less( p, 0, polygons->n_items )
+        connected[p] = (Smallest_int) FALSE;
+
+    included[target_poly] = (Smallest_int) TRUE;
+
+    INITIALIZE_QUEUE( queue );
+
+    neigh_poly = neighbours[0];
+    connected[neigh_poly] = (Smallest_int) TRUE;
+    INSERT_IN_QUEUE( queue, neigh_poly );
+    n_connected = 1;
+
+    while( n_connected < n_neighbours && !IS_QUEUE_EMPTY( queue ) )
+    {
+        REMOVE_FROM_QUEUE( queue, poly );
+
+        size = GET_OBJECT_SIZE( *polygons, poly );
+        for_less( edge, 0, size )
+        {
+            neigh_poly = polygons->neighbours[
+                             POINT_INDEX(polygons->end_indices,poly,edge)];
+
+            if( neigh_poly >= 0 &&
+                !included[neigh_poly] && !connected[neigh_poly] )
+            {
+                for_less( n, 1, n_neighbours )
+                {
+                    if( neigh_poly == neighbours[n] )
+                        ++n_connected;
+                }
+
+                INSERT_IN_QUEUE( queue, neigh_poly );
+                connected[neigh_poly] = (Smallest_int) TRUE;
+            }
+        }
+    }
+
+    DELETE_QUEUE( queue );
+
+    included[target_poly] = (Smallest_int) FALSE;
+
+    return( n_connected == n_neighbours );
+}
+
+private  int   assign_included(
+    polygons_struct    *polygons,
+    Smallest_int       included[] )
+{
+    int                init_poly, poly, neigh_poly, size;
+    int                n_included, edge;
+    Smallest_int       *connected;
+    BOOLEAN            found;
+    QUEUE_STRUCT(int)  queue;
+
+    found = FALSE;
+    init_poly = -1;
+
+    for_less( poly, 0, polygons->n_items )
+    {
+        size = GET_OBJECT_SIZE( *polygons, poly );
+        for_less( edge, 0, size )
+        {
+            neigh_poly = polygons->neighbours[
+                        POINT_INDEX(polygons->end_indices,poly,edge)];
+
+            if( neigh_poly < 0 )
+            {
+                found = TRUE;
+                init_poly = poly;
+                break;
+            }
+        }
+
+        if( found )
+            break;
+    }
+
+    if( !found )
+        init_poly = 0;
+
+    ALLOC( connected, polygons->n_items );
+
+    for_less( poly, 0, polygons->n_items )
+        included[poly] = (Smallest_int) FALSE;
+
+    INITIALIZE_QUEUE( queue );
+    INSERT_IN_QUEUE( queue, init_poly );
+    included[init_poly] = (Smallest_int) TRUE;
+    n_included = 1;
+
+    while( n_included < polygons->n_items / 2 &&
+           !IS_QUEUE_EMPTY( queue ) )
+    {
+        REMOVE_FROM_QUEUE( queue, poly );
+
+        size = GET_OBJECT_SIZE( *polygons, poly );
+        for_less( edge, 0, size )
+        {
+            neigh_poly = polygons->neighbours[
+                             POINT_INDEX(polygons->end_indices,poly,edge)];
+
+            if( neigh_poly >= 0 &&
+                !included[neigh_poly] &&
+                can_include( polygons, neigh_poly, included, connected ) )
+            {
+                INSERT_IN_QUEUE( queue, neigh_poly );
+                included[neigh_poly] = (Smallest_int) TRUE;
+                ++n_included;
+            }
+        }
+    }
+
+    DELETE_QUEUE( queue );
+
+    FREE( connected );
+
+    return( n_included );
+}
+
+private  int   split_polygons(
+    polygons_struct    *polygons,
+    polygons_struct    *out1,
+    polygons_struct    *out2,
+    int                translation1[],
+    int                translation2[] )
+{
+    int                p, ind, poly, vertex, size, point;
+    int                n_included, n_indices[2];
+    Smallest_int       *included;
+    polygons_struct    *out[2];
+    int                *translation[2];
+
+    ALLOC( included, polygons->n_items );
+
+    n_included = assign_included( polygons, included );
+
+    for_less( p, 0, polygons->n_points )
+    {
+        translation1[p] = -1;
+        translation2[p] = -1;
+    }
+
+    out[0] = out1;
+    out[1] = out2;
+    translation[0] = translation1;
+    translation[1] = translation2;
+    n_indices[0] = 0;
+    n_indices[1] = 0;
+
+    for_less( poly, 0, polygons->n_items )
+    {
+        if( included[poly] )
+            ind = 0;
+        else
+            ind = 1;
+
+        size = GET_OBJECT_SIZE( *polygons, poly );
+        for_less( vertex, 0, size )
+        {
+            point = polygons->indices[POINT_INDEX(polygons->end_indices,poly,
+                                                  vertex)];
+
+            if( translation[ind][point] < 0 )
+            {
+                translation[ind][point] = out[ind]->n_points;
+                ADD_ELEMENT_TO_ARRAY( out[ind]->points, out[ind]->n_points,
+                                      polygons->points[point],
+                                      DEFAULT_CHUNK_SIZE );
+                --out[ind]->n_points;
+                ADD_ELEMENT_TO_ARRAY( out[ind]->normals, out[ind]->n_points,
+                                      polygons->normals[point],
+                                      DEFAULT_CHUNK_SIZE );
+            }
+            ADD_ELEMENT_TO_ARRAY( out[ind]->indices, n_indices[ind],
+                                  translation[ind][point], DEFAULT_CHUNK_SIZE );
+        }
+
+        ADD_ELEMENT_TO_ARRAY( out[ind]->end_indices, out[ind]->n_items,
+                              n_indices[ind], DEFAULT_CHUNK_SIZE );
+    }
+
+    FREE( included );
+
+    return( n_included );
+}
