00b0dbdf669823cb9eaccaa05f21dd5022a17422
david <david>
1996-12-16 15:22:59 +0000

    *** empty log message ***

--- manifold_polygons.c
+++ manifold_polygons.c
@@ -56,10 +56,8 @@ int  main(
     delete_polygons( polygons );
     *polygons = out;
 
-/*
     (void) output_graphics_file( output_filename, format, n_objects,
                                  object_list );
-*/
 
     return( 0 );
 }
@@ -70,7 +68,8 @@ private  void   manifold_polygons(
     polygons_struct    *out )
 {
     int                point, poly, size, neigh, neigh_size, v, edge, p;
-    int                current, n_included;
+    int                current, n_points_included, n_polys_included;
+    int                ind, *new_point_ids, n;
     BOOLEAN            add;
     Smallest_int       *point_included, *poly_included;
     QUEUE_STRUCT(int)  queue;
@@ -114,44 +113,45 @@ private  void   manifold_polygons(
             neigh = polygons->neighbours[POINT_INDEX(polygons->end_indices,
                                                      current,edge)];
 
-            if( poly_included[neigh] )
+            if( neigh < 0 || poly_included[neigh] )
                 continue;
 
             neigh_size = GET_OBJECT_SIZE( *polygons, neigh );
-            n_included = 0;
+            n_points_included = 0;
             for_less( v, 0, neigh_size )
             {
                 p = polygons->indices[POINT_INDEX(polygons->end_indices,
                                                   neigh,v)];
                 if( point_included[p] )
-                    ++n_included;
+                    ++n_points_included;
             }
 
-            if( n_included != 2 && n_included != 3 )
+            if( n_points_included != 2 && n_points_included != 3 )
             {
-                print( "N included: %d\n", n_included );
+                print( "N included: %d\n", n_points_included );
                 continue;
             }
 
-            if( n_included == 2 )
+            if( n_points_included == 2 )
                 add = TRUE;
             else
             {
-                n_included = 0;
+                n_polys_included = 0;
                 for_less( v, 0, neigh_size )
                 {
-                    if( poly_included[polygons->neighbours[
-                               POINT_INDEX(polygons->end_indices,neigh,v)]] )
-                        ++n_included;
+                    n = polygons->neighbours[POINT_INDEX(polygons->end_indices,
+                                                         neigh,v)];
+                    if( n < 0 || poly_included[n] )
+                        ++n_polys_included;
                 }
 
-                if( n_included == 0 )
+                if( n_polys_included == 0 )
                 {
-                    print( "N poly included: %d\n", n_included );
+                    print( "N poly included: %d\n", n_polys_included );
                     continue;
                 }
 
-                if( n_included == 1 )
+                if( n_polys_included == 1 )
                     add = FALSE;
                 else
                     add = TRUE;
@@ -173,12 +173,81 @@ private  void   manifold_polygons(
 
     DELETE_QUEUE( queue );
 
-    n_included = 0;
+    n_points_included = 0;
+    for_less( p, 0, polygons->n_points )
+    {
+        if( point_included[p] )
+            ++n_points_included;
+    }
+
+    n_polys_included = 0;
     for_less( poly, 0, polygons->n_items )
     {
         if( poly_included[poly] )
-            ++n_included;
+            ++n_polys_included;
+        else
+        {
+            int  n;
+            size = GET_OBJECT_SIZE( *polygons, poly );
+            n = 0;
+            for_less( v, 0, neigh_size )
+            {
+                n = polygons->neighbours[POINT_INDEX(polygons->end_indices,
+                                                     poly,v)];
+                if( n < 0 || poly_included[n] )
+                    ++n;
+            }
+
+            if( n == 2 || n == 3 )
+                handle_internal_error( "holey" );
+        }
     }
 
-    print( "Polygons labeled: %d / %d\n", n_included, polygons->n_items ); 
+    print( "Polygons labeled: %d / %d\n", n_polys_included, polygons->n_items ); 
+    initialize_polygons( out, WHITE, NULL );
+
+    out->n_points = n_points_included;
+    ALLOC( out->points, n_points_included );
+    ALLOC( out->normals, n_points_included );
+    ALLOC( new_point_ids, polygons->n_points );
+    ind = 0;
+    for_less( p, 0, polygons->n_points )
+    {
+        if( point_included[p] )
+        {
+            new_point_ids[p] = ind;
+            out->points[ind] = polygons->points[p];
+            out->normals[ind] = polygons->normals[p];
+            ++ind;
+        }
+        else
+            new_point_ids[p] = -1;
+    }
+
+    out->n_items = n_polys_included;
+    ALLOC( out->end_indices, n_polys_included );
+    for_less( poly, 0, n_polys_included )
+        out->end_indices[poly] = 3 * (poly+1);
+    ALLOC( out->indices, 3 * n_polys_included );
+
+    ind = 0;
+    for_less( poly, 0, polygons->n_items )
+    {
+        if( !poly_included[poly] )
+            continue;
+
+        size = GET_OBJECT_SIZE( *polygons, poly );
+        for_less( v, 0, size )
+        {
+            p = polygons->indices[POINT_INDEX(polygons->end_indices,poly,v)];
+
+            out->indices[ind] = new_point_ids[p];
+            ++ind;
+        }
+    }
+
+    if( ind != 3 * n_polys_included )
+        handle_internal_error( "ind" );
+
+    FREE( new_point_ids );
 }
