993671acd4d466e661caa714c9091ba3f319e7f6
david <david>
1997-02-10 14:56:10 +0000

    *** empty log message ***

--- polygon_map.c
+++ polygon_map.c
@@ -16,13 +16,14 @@ int  main(
 
     initialize_argument_processing( argc, argv );
 
-    if( !get_string_argument( "", &src_polygons_filename ) ||
-        !get_string_argument( "", &dest_polygons_filename ) ||
-        !get_string_argument( "", &input_filename ) ||
-        !get_string_argument( "", &output_filename ) )
+    if( !get_string_argument( NULL, &src_polygons_filename ) ||
+        !get_string_argument( NULL, &dest_polygons_filename ) )
     {
-        print( "Usage: %s  src_polygons  dest_polygons input_file output_file\n",
+        print_error(
+             "Usage: %s  src_polygons  dest_polygons input_file output_file\n",
                argv[0] );
+        print_error(
+             "        [input2.obj output2.obj] [input3.obj output3.obj]\n" );
         return( 1 );
     }
 
@@ -41,31 +42,36 @@ int  main(
         return( 1 );
     }
 
-    if( input_graphics_file( input_filename,
-                             &format, &n_objects, &objects ) != OK )
-        return( 1 );
-
     create_polygons_bintree( get_polygons_ptr(src_objects[0]),
                              ROUND( (Real) get_polygons_ptr(
                                             src_objects[0])->n_items *
                                     BINTREE_FACTOR ) );
 
-    for_less( i, 0, n_objects )
+    while( get_string_argument( NULL, &input_filename ) &&
+           get_string_argument( NULL, &output_filename ) )
     {
-        n_points = get_object_points( objects[i], &points );
+        if( input_graphics_file( input_filename,
+                                 &format, &n_objects, &objects ) != OK )
+            return( 1 );
 
-        polygon_transform_points( get_polygons_ptr(src_objects[0]),
-                                  get_polygons_ptr(dest_objects[0]),
-                                  n_points, points, points );
+        for_less( i, 0, n_objects )
+        {
+            n_points = get_object_points( objects[i], &points );
+
+            polygon_transform_points( get_polygons_ptr(src_objects[0]),
+                                      get_polygons_ptr(dest_objects[0]),
+                                      n_points, points, points );
+        }
+
+        if( output_graphics_file( output_filename, format, n_objects,
+                                  objects ) != OK )
+            return( 1 );
+
+        delete_object_list( n_objects, objects );
     }
-
-    if( output_graphics_file( output_filename, format, n_objects,
-                              objects ) != OK )
-        return( 1 );
 
     delete_object_list( n_src_objects, src_objects );
     delete_object_list( n_dest_objects, dest_objects );
-    delete_object_list( n_objects, objects );
 
     return( 0 );
 }
