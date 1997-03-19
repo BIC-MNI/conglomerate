81df3a2d9297b8e9d812113e00aa5cbd98854283
david <david>
1997-03-19 01:31:10 +0000

    *** empty log message ***

--- group_diff.c
+++ group_diff.c
@@ -16,21 +16,23 @@ int  main(
     FILE             *file;
     STRING           filename, output_filename;
     int              i, n_objects, n_surfaces[2], group, n_points, s, p;
-    int              v, nu;
+    int              v, nu, which;
     File_formats     format;
     object_struct    **object_list;
     polygons_struct  polygons;
     Point            *points, *copy_points, **samples[2];
     Point            *avg1_points, *avg2_points;
     Vector           offset;
-    Real             **inv_s, tx, ty, tz, mahalanobis;
-    Real             conversion_to_f_statistic;
+    Real             **inv_s, tx, ty, tz, mahalanobis, variance;
+    Real             mean[2], var[2], sum_x, sum_xx, dist;
+    Real             conversion_to_f_statistic, value;
+    BOOLEAN          one_d_flag;
 
     initialize_argument_processing( argc, argv );
 
     if( !get_string_argument( NULL, &output_filename ) )
     {
-        print( "Usage: %s  output.f_stat  file1 file2 + file3 file4 ..\n",
+        print( "Usage: %s  output.f_stat  [1] file1 file2 + file3 file4 ..\n",
                argv[0] );
         return( 1 );
     }
@@ -39,9 +41,16 @@ int  main(
     n_surfaces[1] = 0;
     group = 0;
 
+    one_d_flag = FALSE;
+
     while( get_string_argument( NULL, &filename ) )
     {
-        if( equal_strings( filename, "+" ) )
+        if( equal_strings( filename, "1" ) )
+        {
+            one_d_flag = TRUE;
+            continue;
+        }
+        else if( equal_strings( filename, "+" ) )
         {
             ++group;
             if( group > 1 )
@@ -51,6 +60,8 @@ int  main(
             }
             continue;
         }
+
+        print( "File: %s\n", filename );
 
         if( input_graphics_file( filename, &format, &n_objects,
                                  &object_list ) != OK ||
@@ -108,7 +119,14 @@ int  main(
     {
         SCALE_POINT( avg1_points[i], avg1_points[i], 1.0 / (Real)n_surfaces[0]);
         SCALE_POINT( avg2_points[i], avg2_points[i], 1.0 / (Real)n_surfaces[1]);
+
+        ADD_POINTS( polygons.points[i], avg1_points[i], avg2_points[i] );
+        SCALE_POINT( polygons.points[i], polygons.points[i],
+                     1.0 / (Real) (n_surfaces[0]+n_surfaces[1]) );
     }
+
+    if( one_d_flag )
+        compute_polygon_normals( &polygons );
 
     if( open_file( output_filename, WRITE_FILE, ASCII_FORMAT, &file ) != OK )
         return( 1 );
@@ -122,29 +140,65 @@ int  main(
                                  (1.0 / (Real) n_surfaces[0] +
                                   1.0 / (Real) n_surfaces[1]));
 
-    for_less( p, 0, polygons.n_points )
+    for_less( p, 0, n_points )
     {
-        compute_inv_variance( n_surfaces, samples, p, &avg1_points[p],
-                              &avg2_points[p], inv_s );
+        if( one_d_flag )
+        {
+            for_less( which, 0, 2 )
+            {
+                sum_x = 0.0;
+                sum_xx = 0.0;
+                for_less( s, 0, n_surfaces[which] )
+                {
+                    SUB_POINTS( offset, samples[which][s][p],
+                                polygons.points[p] );
+                    dist = DOT_VECTORS( offset, polygons.normals[p] );
+                    sum_x += dist;
+                    sum_xx += dist * dist;
+                }
 
-        SUB_POINTS( offset, avg1_points[p], avg2_points[p] );
+                mean[which] = sum_x / (Real) n_surfaces[which];
 
-        tx = (Real) Vector_x(offset) * inv_s[0][0] +
-             (Real) Vector_y(offset) * inv_s[1][0] +
-             (Real) Vector_z(offset) * inv_s[2][0];
-        ty = (Real) Vector_x(offset) * inv_s[0][1] +
-             (Real) Vector_y(offset) * inv_s[1][1] +
-             (Real) Vector_z(offset) * inv_s[2][1];
-        tz = (Real) Vector_x(offset) * inv_s[0][2] +
-             (Real) Vector_y(offset) * inv_s[1][2] +
-             (Real) Vector_z(offset) * inv_s[2][2];
+                if( n_surfaces[which] == 1 )
+                    var[which] = 0.0;
+                else
+                    var[which] = (sum_xx - sum_x * sum_x /
+                       (Real) n_surfaces[which]) / (Real) n_surfaces[which];
+            }
 
-        mahalanobis = tx * (Real) Vector_x(offset) +
-                      ty * (Real) Vector_y(offset) +
-                      tz * (Real) Vector_z(offset);
+            variance = sqrt( ((Real) n_surfaces[0] * var[0] +
+                              (Real) n_surfaces[1] * var[1]) /
+                             (Real) v );
 
+            value = (mean[0] - mean[1]) /
+                       (variance * sqrt( 1.0 / (Real) n_surfaces[0] +
+                                         1.0 / (Real) n_surfaces[1] ));
+        }
+        else
+        {
+            compute_inv_variance( n_surfaces, samples, p, &avg1_points[p],
+                                  &avg2_points[p], inv_s );
 
-        (void) output_real( file, mahalanobis * conversion_to_f_statistic );
+            SUB_POINTS( offset, avg1_points[p], avg2_points[p] );
+
+            tx = (Real) Vector_x(offset) * inv_s[0][0] +
+                 (Real) Vector_y(offset) * inv_s[1][0] +
+                 (Real) Vector_z(offset) * inv_s[2][0];
+            ty = (Real) Vector_x(offset) * inv_s[0][1] +
+                 (Real) Vector_y(offset) * inv_s[1][1] +
+                 (Real) Vector_z(offset) * inv_s[2][1];
+            tz = (Real) Vector_x(offset) * inv_s[0][2] +
+                 (Real) Vector_y(offset) * inv_s[1][2] +
+                 (Real) Vector_z(offset) * inv_s[2][2];
+
+            mahalanobis = tx * (Real) Vector_x(offset) +
+                          ty * (Real) Vector_y(offset) +
+                          tz * (Real) Vector_z(offset);
+
+            value = mahalanobis * conversion_to_f_statistic;
+        }
+
+        (void) output_real( file, value );
         (void) output_newline( file );
     }
 
