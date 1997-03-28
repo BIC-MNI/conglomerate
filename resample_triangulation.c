81f1ea37dbbbb97e9662f3a202765c00e386ff5e
david <david>
1997-03-28 02:13:39 +0000

    *** empty log message ***

--- resample_triangulation.c
+++ resample_triangulation.c
@@ -9,17 +9,22 @@ typedef struct tri_node_struct
 
 typedef struct
 {
-    hash_table_struct  edge_hash;
     int                n_points;
     Point              *points;
-    Smallest_int       *point_usage;
     int                n_triangles;
     tri_node_struct    *triangles;
 } tri_mesh_struct;
 
+private   int   get_tri_mesh_n_triangles(
+    tri_mesh_struct   *mesh );
+
 private  void   convert_polygons_to_mesh(
     polygons_struct  *polygons,
     tri_mesh_struct  *mesh );
+
+private   void   convert_mesh_to_polygons(
+    tri_mesh_struct   *mesh,
+    polygons_struct   *polygons );
 
 private  Status  output_triangular_mesh(
     STRING         filename,
@@ -31,11 +36,16 @@ private  Status  input_triangular_mesh(
     File_formats     format,
     tri_mesh_struct  *mesh );
 
+private   void   resample_mesh(
+    tri_mesh_struct   *mesh,
+    Real              min_size,
+    Real              max_size );
+
 private  void  usage(
     STRING   executable )
 {
     STRING  usage_str = "\n\
-Usage: %s  input.obj  output.obj  min_size max_size in.msh out.msh in\n\
+Usage: %s  input.obj|input.msh  output.obj  output.msh min_size max_size\n\
 \n\
      Subdivides the triangular mesh.\n\h";
 
@@ -47,10 +57,10 @@ int  main(
     char   *argv[] )
 {
     STRING           input_filename, output_filename;
-    STRING           input_mesh_filename, output_mesh_filename;
-    int              n_objects;
+    STRING           output_mesh_filename;
+    int              n_objects, prev_n_polys, new_n_polys;
     File_formats     format;
-    object_struct    **object_list;
+    object_struct    **object_list, *object;
     polygons_struct  *polygons;
     Real             min_size, max_size;
     BOOLEAN          input_mesh_specified;
@@ -60,44 +70,52 @@ int  main(
 
     if( !get_string_argument( NULL, &input_filename ) ||
         !get_string_argument( NULL, &output_filename ) ||
+        !get_string_argument( NULL, &output_mesh_filename ) ||
         !get_real_argument( 0.0, &min_size ) ||
-        !get_real_argument( 0.0, &max_size ) ||
-        !get_string_argument( NULL, &input_mesh_filename ) ||
-        !get_string_argument( NULL, &output_mesh_filename ) )
+        !get_real_argument( 0.0, &max_size ) )
     {
         usage( argv[0] );
         return( 1 );
     }
 
-    input_mesh_specified = !equal_strings( input_mesh_filename, "-" );
-
-    if( input_graphics_file( input_filename, &format, &n_objects,
-                             &object_list ) != OK ||
-        n_objects < 1 || get_object_type(object_list[0]) != POLYGONS )
-    {
-        print_error( "Error in input file.\n" );
-        return( 1 );
-    }
-
-    polygons = get_polygons_ptr( object_list[0] );
+    input_mesh_specified = filename_extension_matches( input_filename, "msh" );
 
     if( input_mesh_specified )
     {
-        if( input_triangular_mesh( input_mesh_filename, BINARY_FORMAT,
+        if( input_triangular_mesh( input_filename, BINARY_FORMAT,
                                    &mesh ) != OK )
             return( 1 );
     }
     else
     {
-        convert_polygons_to_mesh( polygons, &mesh );
-    }
-    
-/*
-    resample_mesh( polygons, &mesh, min_size, max_size );
-*/
+        if( input_graphics_file( input_filename, &format, &n_objects,
+                                 &object_list ) != OK ||
+            n_objects < 1 || get_object_type(object_list[0]) != POLYGONS )
+        {
+            print_error( "Error in input file.\n" );
+            return( 1 );
+        }
 
-    (void) output_graphics_file( output_filename, format,
-                                 n_objects, object_list );
+        polygons = get_polygons_ptr( object_list[0] );
+
+        convert_polygons_to_mesh( polygons, &mesh );
+        delete_polygons( polygons );
+    }
+
+    prev_n_polys = get_tri_mesh_n_triangles( &mesh );
+    
+    resample_mesh( &mesh, min_size, max_size );
+
+    object = create_object( POLYGONS );
+    polygons = get_polygons_ptr( object );
+    convert_mesh_to_polygons( &mesh, polygons );
+
+    new_n_polys = polygons->n_items;
+
+    print( "Resampled %d polygons into %d polygons.\n", prev_n_polys,
+           new_n_polys );
+
+    (void) output_graphics_file( output_filename, format, 1, &object );
 
     (void) output_triangular_mesh( output_mesh_filename, BINARY_FORMAT, &mesh );
 
@@ -109,7 +127,6 @@ int  main(
 private  void  initialize_tri_mesh(
     tri_mesh_struct  *mesh )
 {
-    initialize_hash_table( &mesh->edge_hash, 10000, 2*sizeof(int), 0.5, 0.25 );
     mesh->n_triangles = 0;
     mesh->n_points = 0;
 }
@@ -132,10 +149,6 @@ private  void  tri_mesh_insert_triangle(
 
     ADD_ELEMENT_TO_ARRAY( mesh->triangles, mesh->n_triangles, node,
                           DEFAULT_CHUNK_SIZE );
-
-    ++mesh->point_usage[p0];
-    ++mesh->point_usage[p1];
-    ++mesh->point_usage[p2];
 }
 
 private  int  tri_mesh_insert_point(
@@ -145,85 +158,11 @@ private  int  tri_mesh_insert_point(
     int  ind;
 
     ind = mesh->n_points;
-    SET_ARRAY_SIZE( mesh->point_usage, mesh->n_points, mesh->n_points+1,
-                    DEFAULT_CHUNK_SIZE );
-    mesh->point_usage[ind] = 0;
 
     ADD_ELEMENT_TO_ARRAY( mesh->points, mesh->n_points, *point,
                           DEFAULT_CHUNK_SIZE )
 
     return( ind );
-}
-
-private  int  get_key(
-    int  p0,
-    int  p1 )
-{
-    int   k0, k1;
-
-    if( p0 >= (1<<16) || p1 >= (1<<16) )
-        print( "Warning: get_key, too many points.\n" );
-
-    k0 = MIN( p0, p1 );
-    k1 = MAX( p0, p1 );
-
-    return( (k0 << 16) + k1 );
-}
-
-private  void  insert_edge_midpoint(
-    tri_mesh_struct  *mesh,
-    int              p0,
-    int              p1,
-    int              midpoint )
-{
-    int     key;
-
-    key = get_key( p0, p1 );
-
-    if( lookup_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint ) )
-        return;
-
-    insert_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint );
-}
-
-private  int  get_edge_midpoint(
-    tri_mesh_struct  *mesh,
-    int              p0,
-    int              p1 )
-{
-    int     key, midpoint;
-    Point   mid;
-
-    key = get_key( p0, p1 );
-
-    midpoint = 0;  /* to avoid compiler message*/
-
-    if( lookup_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint ) )
-        return( midpoint );
-
-    INTERPOLATE_POINTS( mid, mesh->points[p0], mesh->points[p1], 0.5 );
-
-    midpoint = tri_mesh_insert_point( mesh, &mid );
-
-    insert_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint );
-
-    return( midpoint );
-}
-
-private  void  subdivide_tri_node(
-    tri_mesh_struct   *mesh,
-    tri_node_struct   *node )
-{
-    int  midpoints[3];
-
-    midpoints[0] = get_edge_midpoint( mesh, node->nodes[0], node->nodes[1] );
-    midpoints[1] = get_edge_midpoint( mesh, node->nodes[1], node->nodes[2] );
-    midpoints[2] = get_edge_midpoint( mesh, node->nodes[2], node->nodes[0] );
-
-    tri_mesh_insert_triangle( mesh, node->nodes[0], midpoints[0], midpoints[2]);
-    tri_mesh_insert_triangle( mesh, midpoints[0], node->nodes[1], midpoints[1]);
-    tri_mesh_insert_triangle( mesh, midpoints[0], midpoints[1], midpoints[2]);
-    tri_mesh_insert_triangle( mesh, midpoints[2], midpoints[1], node->nodes[2]);
 }
 
 private  Status  output_tri_node(
@@ -302,10 +241,6 @@ private  Status  input_tri_node(
     node->nodes[1] = list[1];
     node->nodes[2] = list[2];
 
-    ++mesh->point_usage[list[0]];
-    ++mesh->point_usage[list[1]];
-    ++mesh->point_usage[list[2]];
-
     FREE( list );
 
     if( leaf_flag )
@@ -325,13 +260,6 @@ private  Status  input_tri_node(
                 != OK )
                 return( ERROR );
         }
-
-        insert_edge_midpoint( mesh, node->nodes[0], node->nodes[1],
-                              node->children[2]->nodes[0] );
-        insert_edge_midpoint( mesh, node->nodes[1], node->nodes[2],
-                              node->children[2]->nodes[1] );
-        insert_edge_midpoint( mesh, node->nodes[2], node->nodes[0],
-                              node->children[2]->nodes[2] );
     }
 
     return( OK );
@@ -343,7 +271,7 @@ private  Status  input_triangular_mesh(
     tri_mesh_struct  *mesh )
 {
     FILE   *file;
-    int    tri, point;
+    int    tri;
 
     initialize_tri_mesh( mesh );
 
@@ -354,10 +282,6 @@ private  Status  input_triangular_mesh(
         return( ERROR );
 
     SET_ARRAY_SIZE( mesh->points, 0, mesh->n_points, DEFAULT_CHUNK_SIZE );
-    SET_ARRAY_SIZE( mesh->point_usage, 0, mesh->n_points, DEFAULT_CHUNK_SIZE );
-
-    for_less( point, 0, mesh->n_points )
-        mesh->point_usage[point] = 0;
 
     if( io_points( file, READ_FILE, format, mesh->n_points, &mesh->points )
                                               != OK )
@@ -389,10 +313,10 @@ private  void   mark_points_used(
 
     if( node->children[0] != NULL )
     {
-        mark_points_used( &node->children[0], used );
-        mark_points_used( &node->children[1], used );
-        mark_points_used( &node->children[2], used );
-        mark_points_used( &node->children[3], used );
+        mark_points_used( node->children[0], used );
+        mark_points_used( node->children[1], used );
+        mark_points_used( node->children[2], used );
+        mark_points_used( node->children[3], used );
     }
 }
 
@@ -406,17 +330,17 @@ private  void   renumber_points(
 
     if( node->children[0] != NULL )
     {
-        renumber_points( &node->children[0], new_id );
-        renumber_points( &node->children[1], new_id );
-        renumber_points( &node->children[2], new_id );
-        renumber_points( &node->children[3], new_id );
+        renumber_points( node->children[0], new_id );
+        renumber_points( node->children[1], new_id );
+        renumber_points( node->children[2], new_id );
+        renumber_points( node->children[3], new_id );
     }
 }
 
 private  void   delete_unused_nodes(
     tri_mesh_struct  *mesh )
 {
-    int   *new_id, point, tri;
+    int   *new_id, point, tri, new_n_points;
 
     ALLOC( new_id, mesh->n_points );
 
@@ -445,6 +369,10 @@ private  void   delete_unused_nodes(
 
     for_less( point, 0, mesh->n_points )
         mesh->points[new_id[point]] = mesh->points[point];
+
+    SET_ARRAY_SIZE( mesh->points, mesh->n_points, new_n_points,
+                    DEFAULT_CHUNK_SIZE );
+    mesh->n_points = new_n_points;
 
     for_less( tri, 0, mesh->n_triangles )
         renumber_points( &mesh->triangles[tri], new_id );
@@ -476,4 +404,166 @@ private  void   convert_polygons_to_mesh(
                  polygons->indices[POINT_INDEX(polygons->end_indices,poly,1)],
                  polygons->indices[POINT_INDEX(polygons->end_indices,poly,2)] );
     }
+}
+
+#ifdef NOT
+    initialize_hash_table( &mesh->edge_hash, 10000, 2*sizeof(int), 0.5, 0.25 );
+
+private  int  get_key(
+    int  p0,
+    int  p1 )
+{
+    int   k0, k1;
+
+    if( p0 >= (1<<16) || p1 >= (1<<16) )
+        print( "Warning: get_key, too many points.\n" );
+
+    k0 = MIN( p0, p1 );
+    k1 = MAX( p0, p1 );
+
+    return( (k0 << 16) + k1 );
+}
+
+private  void  insert_edge_midpoint(
+    tri_mesh_struct  *mesh,
+    int              p0,
+    int              p1,
+    int              midpoint )
+{
+    int     key;
+
+    key = get_key( p0, p1 );
+
+    if( lookup_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint ) )
+        return;
+
+    insert_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint );
+}
+
+private  int  get_edge_midpoint(
+    tri_mesh_struct  *mesh,
+    int              p0,
+    int              p1 )
+{
+    int     key, midpoint;
+    Point   mid;
+
+    key = get_key( p0, p1 );
+
+    midpoint = 0;  /* to avoid compiler message*/
+
+    if( lookup_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint ) )
+        return( midpoint );
+
+    INTERPOLATE_POINTS( mid, mesh->points[p0], mesh->points[p1], 0.5 );
+
+    midpoint = tri_mesh_insert_point( mesh, &mid );
+
+    insert_in_hash_table( &mesh->edge_hash, key, (void *) &midpoint );
+
+    return( midpoint );
+}
+
+private  void  subdivide_tri_node(
+    tri_mesh_struct   *mesh,
+    tri_node_struct   *node )
+{
+    int  midpoints[3];
+
+    midpoints[0] = get_edge_midpoint( mesh, node->nodes[0], node->nodes[1] );
+    midpoints[1] = get_edge_midpoint( mesh, node->nodes[1], node->nodes[2] );
+    midpoints[2] = get_edge_midpoint( mesh, node->nodes[2], node->nodes[0] );
+
+    tri_mesh_insert_triangle( mesh, node->nodes[0], midpoints[0], midpoints[2]);
+    tri_mesh_insert_triangle( mesh, midpoints[0], node->nodes[1], midpoints[1]);
+    tri_mesh_insert_triangle( mesh, midpoints[0], midpoints[1], midpoints[2]);
+    tri_mesh_insert_triangle( mesh, midpoints[2], midpoints[1], node->nodes[2]);
+}
+#endif
+
+private  int  count_triangles(
+    tri_node_struct  *node )
+{
+    int   n_triangles;
+
+    n_triangles = 1;
+
+    if( node->children[0] != NULL )
+    {
+        n_triangles += count_triangles( node->children[0] );
+        n_triangles += count_triangles( node->children[1] );
+        n_triangles += count_triangles( node->children[2] );
+        n_triangles += count_triangles( node->children[3] );
+    }
+
+    return( n_triangles );
+}
+
+private   int   get_tri_mesh_n_triangles(
+    tri_mesh_struct   *mesh )
+{
+    int    tri, n_triangles;
+
+    n_triangles = 0;
+
+    for_less( tri, 0, mesh->n_triangles )
+        n_triangles += count_triangles( &mesh->triangles[tri] );
+
+    return( n_triangles );
+}
+
+private  void   add_to_polygons(
+    int              indices[],
+    int              *current_poly,
+    tri_node_struct  *node )
+{
+    indices[3 * (*current_poly) + 0] = node->nodes[0];
+    indices[3 * (*current_poly) + 1] = node->nodes[1];
+    indices[3 * (*current_poly) + 2] = node->nodes[2];
+
+    ++(*current_poly);
+
+    if( node->children[0] != NULL )
+    {
+        add_to_polygons( indices, current_poly, node->children[0] );
+        add_to_polygons( indices, current_poly, node->children[1] );
+        add_to_polygons( indices, current_poly, node->children[2] );
+        add_to_polygons( indices, current_poly, node->children[3] );
+    }
+}
+
+private   void   convert_mesh_to_polygons(
+    tri_mesh_struct   *mesh,
+    polygons_struct   *polygons )
+{
+    int    poly, tri;
+
+    initialize_polygons( polygons, WHITE, NULL );
+
+    ALLOC( polygons->points, mesh->n_points );
+    ALLOC( polygons->normals, mesh->n_points );
+    polygons->n_points = mesh->n_points;
+
+    polygons->n_items = get_tri_mesh_n_triangles( mesh );
+
+    ALLOC( polygons->end_indices, polygons->n_items );
+    for_less( poly, 0, polygons->n_items )
+        polygons->end_indices[poly] = 3 * (poly+1);
+
+    ALLOC( polygons->indices, 3 * polygons->n_items );
+
+    poly = 0;
+
+    for_less( tri, 0, mesh->n_triangles )
+        add_to_polygons( polygons->indices, &poly, &mesh->triangles[tri] );
+
+    compute_polygon_normals( polygons );
+}
+
+private   void   resample_mesh(
+    tri_mesh_struct   *mesh,
+    Real              min_size,
+    Real              max_size )
+{
+    delete_unused_nodes( mesh );
 }
