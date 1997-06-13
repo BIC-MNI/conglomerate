c0f8690bff9b99e3eb5ed86692e8f034aff7db11
david <david>
1997-06-13 16:10:10 +0000

    *** empty log message ***

--- surface_mask2.c
+++ surface_mask2.c
@@ -5,6 +5,15 @@
 
 #define  OFFSET   1000.0
 
+private  BOOLEAN  get_extrapolated_value(
+    Volume         volume,
+    int            x,
+    int            y,
+    int            z,
+    object_struct  *surface,
+    Real           threshold,
+    Real           *value_to_set );
+
 int  main(
     int   argc,
     char  *argv[] )
@@ -13,8 +22,9 @@ int  main(
     STRING               output_volume_filename;
     Real                 set_value, separations[MAX_DIMENSIONS];
     Real                 min_value, max_value, value;
-    Real                 xw, yw, zw;
+    Real                 xw, yw, zw, threshold, value_to_set;
     Point                origin, dest;
+    Real                 min_real_value, max_real_value;
     Vector               direction;
     STRING               history;
     File_formats         format;
@@ -26,6 +36,7 @@ int  main(
     Real                 dist, *distances, tmp, voxel[MAX_DIMENSIONS];
     object_struct        **objects;
     BOOLEAN              set_value_specified, inside, erase_flag;
+    BOOLEAN              threshold_specified;
     progress_struct      progress;
 
     initialize_argument_processing( argc, argv );
@@ -37,13 +48,14 @@ int  main(
         print_error(
             "Usage: %s  in_volume.mnc  in_surface.obj  out_volume.mnc\n",
                argv[0] );
-        print_error( "      [min_value max_value] [set_value]\n" );
+        print_error( "      [min_value max_value] [set_value] [threshold]\n" );
         return( 1 );
     }
 
     (void) get_real_argument( 0.0, &min_value );
     (void) get_real_argument( min_value - 1.0, &max_value );
     set_value_specified = get_real_argument( 0.0, &set_value );
+    threshold_specified = get_real_argument( 0.0, &threshold );
 
     if( input_volume( input_volume_filename, 3, XYZ_dimension_names,
                       NC_UNSPECIFIED, FALSE, 0.0, 0.0,
@@ -63,8 +75,15 @@ int  main(
     get_volume_separations( volume, separations );
     get_volume_sizes( volume, sizes );
 
+    min_real_value = get_volume_real_min( volume );
+    max_real_value = get_volume_real_max( volume );
+
     if( !set_value_specified )
-        set_value = get_volume_real_min( volume );
+        set_value = min_real_value;
+    else if( set_value < min_real_value )
+        set_value = min_real_value;
+    else if( set_value > max_real_value )
+        set_value = max_real_value;
 
     create_polygons_bintree( get_polygons_ptr(objects[0]),
                   ROUND( (Real) get_polygons_ptr(objects[0])->n_items *
@@ -138,7 +157,21 @@ int  main(
                     }
 
                     if( erase_flag )
-                        set_volume_real_value( volume, x, y, z, 0, 0,set_value);
+                    {
+                        if( !threshold_specified ||
+                            !get_extrapolated_value( volume, x, y, z,
+                                        objects[0], threshold, &value_to_set ) )
+                        {
+                            value_to_set = set_value;
+                        }
+                        else if( value_to_set < min_real_value )
+                            value_to_set = min_real_value;
+                        else if( value_to_set > max_real_value )
+                            value_to_set = max_real_value;
+
+                        set_volume_real_value( volume, x, y, z, 0, 0,
+                                               value_to_set );
+                    }
                 }
             }
             else
@@ -166,4 +199,111 @@ int  main(
     delete_volume( volume );
 
     return( 0 );
+}
+
+private  BOOLEAN  get_one_extrapolated_value(
+    Volume         volume,
+    int            x1,
+    int            y1,
+    int            z1,
+    int            x2,
+    int            y2,
+    int            z2,
+    object_struct  *surface,
+    Real           threshold,
+    Real           *value_to_set )
+{
+    int      sizes[N_DIMENSIONS];
+    int      obj_index, n_int, n_intersections, i;
+    Real     value2, dist, *distances, xw, yw, zw;
+    Real     voxel[N_DIMENSIONS];
+    Point    p1, p2;
+    Vector   direction;
+
+    get_volume_sizes( volume, sizes );
+
+    if( x2 < 0 || x1 >= sizes[X] ||
+        y2 < 0 || y1 >= sizes[Y] ||
+        z2 < 0 || z1 >= sizes[Z] )
+        return( FALSE );
+
+    voxel[X] = (Real) x1;
+    voxel[Y] = (Real) y1;
+    voxel[Z] = (Real) z1;
+    convert_voxel_to_world( volume, voxel, &xw, &yw, &zw );
+    fill_Point( p1, xw, yw, zw );
+
+    voxel[X] = (Real) x2;
+    voxel[Y] = (Real) y2;
+    voxel[Z] = (Real) z2;
+    convert_voxel_to_world( volume, voxel, &xw, &yw, &zw );
+    fill_Point( p2, xw, yw, zw );
+    SUB_POINTS( direction, p2, p1 );
+
+    n_intersections = intersect_ray_with_object( &p1, &direction,
+                                                 surface, &obj_index, &dist,
+                                                 &distances );
+
+    n_int = 0;
+    for_less( i, 0, n_intersections )
+    {
+        if( distances[i] > 0.0 && distances[i] < 1.0 )
+        {
+            ++n_int;
+            dist = distances[i];
+        }
+    }
+
+    if( n_intersections > 0 )
+        FREE( distances );
+
+    if( n_int != 1 )
+        return( FALSE );
+
+    value2 = get_volume_real_value( volume, x2, y2, z2, 0, 0 );
+
+    if( value2 <= threshold )
+        return( FALSE );
+
+    *value_to_set = (threshold - value2 * dist) / (1.0 - dist);
+
+    return( TRUE );
+}
+
+private  BOOLEAN  get_extrapolated_value(
+    Volume         volume,
+    int            x,
+    int            y,
+    int            z,
+    object_struct  *surface,
+    Real           threshold,
+    Real           *value_to_set )
+{
+    Real     value;
+    int      c, dir, n_bounds, voxel[N_DIMENSIONS];
+
+    *value_to_set = 0.0;
+    n_bounds = 0;
+    for_less( c, 0, N_DIMENSIONS )
+    {
+        for( dir = -1;  dir <= 1;  dir += 2 )
+        {
+            voxel[X] = x;
+            voxel[Y] = y;
+            voxel[Z] = z;
+            voxel[c] += dir;
+            if( get_one_extrapolated_value( volume, x, y, z,
+                                            voxel[X], voxel[Y], voxel[Z],
+                                            surface, threshold, &value ) )
+            {
+                *value_to_set += value;
+                ++n_bounds;
+            }
+        }
+    }
+
+    if( n_bounds > 0 )
+        *value_to_set /= (Real) n_bounds;
+
+    return( n_bounds > 0 );
 }
