08bda450afd67c2545139652f83598bde5211c3a
david <david>
1997-12-10 12:09:46 +0000

    *** empty log message ***

--- composite_images.c
+++ composite_images.c
@@ -7,6 +7,7 @@ int  main(
 {
     STRING            input_filename, output_filename;
     int               x, y, n_files, r, g, b, a;
+    int               x_offset, y_offset, x_min, x_max, y_min, y_max;
     pixels_struct     pixels, top_pixels;
     Colour            bottom, top, col;
     BOOLEAN           add_flag;
@@ -15,7 +16,7 @@ int  main(
 
     if( argc <= 1 )
     {
-        print( "Usage: %s output.rgb [-add] input1.rgb  [input2.rgb] ...\n", argv[0] );
+        print( "Usage: %s output.rgb [-add] input1.rgb  -xy [input2.rgb] ...\n", argv[0] );
         return( 1 );
     }
 
@@ -23,6 +24,8 @@ int  main(
 
     n_files = 0;
     add_flag = FALSE;
+    x_offset = 0;
+    y_offset = 0;
 
     while( get_string_argument( NULL, &input_filename ) )
     {
@@ -36,7 +39,16 @@ int  main(
             add_flag = FALSE;
             continue;
         }
-
+        if( equal_strings( input_filename, "-xy" ) )
+        {
+            if( !get_int_argument( 0, &x_offset ) ||
+                !get_int_argument( 0, &y_offset ) )
+            {
+                print_error( "Error in -xy arguments\n" );
+                return( 1 );
+            }
+            continue;
+        }
         if( output_filename == NULL )
         {
             output_filename = input_filename;
@@ -56,19 +68,17 @@ int  main(
             pixels = top_pixels;
         else
         {
-            if( top_pixels.x_size != pixels.x_size ||
-                top_pixels.y_size != pixels.y_size )
-            {
-                print_error( "Error in pixels sizes\n" );
-                return( 1 );
-            }
+            x_min = MAX( 0, x_offset );
+            x_max = MIN( pixels.x_size-1, x_offset + top_pixels.x_size - 1 );
+            y_min = MAX( 0, y_offset );
+            y_max = MIN( pixels.y_size-1, y_offset + top_pixels.y_size - 1 );
 
-            for_less( x, 0, pixels.x_size )
+            for_inclusive( x, x_min, x_max )
             {
-                for_less( y, 0, pixels.y_size )
+                for_inclusive( y, y_min, y_max )
                 {
                     bottom = PIXEL_RGB_COLOUR(pixels,x,y);
-                    top = PIXEL_RGB_COLOUR(top_pixels,x,y);
+                    top = PIXEL_RGB_COLOUR(top_pixels,x-x_offset,y-y_offset);
 
                     if( add_flag )
                     {
