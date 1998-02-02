8de2bc2188cfceae65223d71e5837a49392febf8
david <david>
1998-02-02 20:26:35 +0000

    *** empty log message ***

--- image_io.c
+++ image_io.c
@@ -8,44 +8,25 @@ public  Status  get_image_file_size(
     int            *nc )
 {
     char   command[EXTREMELY_LARGE_STRING_SIZE+1];
-    int    z_size;
-    STRING line;
     FILE   *file;
 
     /*---  get the image size */
 
-    (void) sprintf( command, "imginfo %s", filename );
+    (void) sprintf( command, "get_image_size.pl %s", filename );
 
     if( (file = popen( command, "r")) == NULL )
     {
-        print_error( "Error opening file %s or finding command 'imginfo'\n",
+        print_error( "Error opening file %s or finding command 'get_image_size.pl'\n",
                      filename );
         return( ERROR );
     }
 
-    if( input_line( file, &line ) != OK )
-        return( ERROR );
-
-    delete_string( line );
-
-    if( input_line( file, &line ) != OK )
+    if( input_int( file, x_size ) != OK ||
+        input_int( file, y_size ) != OK ||
+        input_int( file, nc ) != OK )
         return( ERROR );
 
     pclose( file );
-
-    if( sscanf( line, "Dimensions (x,y,z,c):	%d, %d, %d, %d",
-                x_size, y_size, &z_size, nc ) != 4 )
-    {
-        print_error( "Error reading size from file %s\n", filename );
-        return( ERROR );
-    }
-
-    if( z_size != 1 )
-    {
-        print_error( "Cannot handle case where z != 1 in input %s\n",
-                     filename );
-        return( ERROR );
-    }
 
     return( OK );
 }
@@ -112,22 +93,90 @@ public  Status  read_image_file(
     return( OK );
 }
 
+public  Status  write_image_file(
+    STRING         filename,
+    pixels_struct  *pixels )
+{
+    char           command[EXTREMELY_LARGE_STRING_SIZE+1];
+    int            x_size, y_size, x, y;
+    FILE           *file;
+    unsigned char  colour[4];
+    Colour         col;
+
+    x_size = pixels->x_size;
+    y_size = pixels->y_size;
+
+    if( find_character(filename,':') < 0 &&
+        string_ends_in( filename, ".rgb" ) )
+    {
+        (void) sprintf( command, "convert -size %dx%d RGBA:- SGI:%s",
+                        x_size, y_size, filename );
+    }
+    else
+    {
+        (void) sprintf( command, "convert -size %dx%d RGBA:- %s",
+                        x_size, y_size, filename );
+    }
+
+    if( (file = popen( command, "w")) == NULL )
+    {
+        print_error( "Error opening file %s or finding command 'convert'\n",
+                     filename );
+        return( ERROR );
+    }
+
+    for_less( y, 0, y_size )
+    {
+        for_less( x, 0, x_size )
+        {
+            col = PIXEL_RGB_COLOUR(*pixels,x,y);
+
+            colour[0] = (unsigned char) get_Colour_r( col );
+            colour[1] = (unsigned char) get_Colour_g( col );
+            colour[2] = (unsigned char) get_Colour_b( col );
+            colour[3] = (unsigned char) get_Colour_a( col );
+
+            if( io_binary_data( file, WRITE_FILE, &colour, sizeof(colour[0]),
+                                4 ) != OK )
+            {
+                print( "Error writing pixel[%d][%d]\n", x, y );
+                return( ERROR );
+            }
+        }
+    }
+
+    pclose( file );
+
+    return( OK );
+}
+
 int  main(
     int   argc,
     char  *argv[] )
 {
-    STRING          input_filename; 
+    STRING          input_filename, output_filename;
     pixels_struct   pixels;
 
     initialize_argument_processing( argc, argv );
 
-    if( !get_string_argument( NULL, &input_filename ) )
+    if( !get_string_argument( NULL, &input_filename ) ||
+        !get_string_argument( NULL, &output_filename ) )
     {
-        print_error( "Need an argument.\n" );
+        print_error( "Usage: %s  input output\n", argv[0] );
         return( 1 );
     }
 
-    (void) read_image_file( input_filename, &pixels );
+    if( read_image_file( input_filename, &pixels ) != OK )
+    {
+        print_error( "Error on input.\n" );
+        return( 1 );
+    }
+
+    if( write_image_file( output_filename, &pixels ) != OK )
+    {
+        print_error( "Error on output.\n" );
+        return( 1 );
+    }
 
     return( 0 );
 }
