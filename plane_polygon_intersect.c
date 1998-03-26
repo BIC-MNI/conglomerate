71253f0b49f20945dad8f1921716481e9580e605
david <david>
1998-03-26 20:46:01 +0000

    *** empty log message ***

--- plane_polygon_intersect.c
+++ plane_polygon_intersect.c
@@ -1,12 +1,6 @@
 #include  <internal_volume_io.h>
 #include  <bicpl.h>
 
-private  void   intersect_planes_with_polygons(
-    polygons_struct   *polygons,
-    Point             *plane_origin,
-    Vector            *plane_normal,
-    lines_struct      *lines );
-
 int  main(
     int   argc,
     char  *argv[] )
@@ -60,8 +54,8 @@ int  main(
         fill_Point( plane_origin, 0.0, 0.0, 0.0 );
         fill_Vector( plane_normal, 0.0, 0.0, 0.0 );
 
-        Point_coord( plane_origin, axis ) = position;
-        Vector_coord( plane_normal, axis ) = 1.0;
+        Point_coord( plane_origin, axis ) = (Point_coord_type) position;
+        Vector_coord( plane_normal, axis ) = (Point_coord_type) 1.0;
     }
 
     if( input_graphics_file( src_polygons_filename,
@@ -91,57 +85,4 @@ int  main(
     delete_object_list( n_dest_objects, dest_objects );
 
     return( 0 );
-}
-
-#define  MAX_POINTS  1000
-
-private  void   intersect_planes_with_polygons(
-    polygons_struct   *polygons,
-    Point             *plane_origin,
-    Vector            *plane_normal,
-    lines_struct      *lines )
-{
-    int     n_points;
-    int     poly, edge, size;
-    int     point_index1, point_index2;
-    Point   points[2], int_point;
-
-    initialize_lines( lines, WHITE );
-
-    for_less( poly, 0, polygons->n_items )
-    {
-        n_points = 0;
-
-        size = GET_OBJECT_SIZE( *polygons, poly );
-
-        for_less( edge, 0, size )
-        {
-            point_index1 = polygons->indices[
-                     POINT_INDEX(polygons->end_indices,poly,edge)];
-            point_index2 = polygons->indices[
-                     POINT_INDEX(polygons->end_indices,poly,(edge+1)%size)];
-
-            if( line_segment_intersects_plane( &polygons->points[point_index1],
-                                               &polygons->points[point_index2],
-                                               plane_origin, plane_normal,
-                                               &int_point ) )
-            {
-                if( n_points == 2 )
-                {
-                    n_points = 0;
-                    break;
-                }
-
-                points[n_points] = int_point;
-                ++n_points;
-            }
-        }
-
-        if( n_points == 2 )
-        {
-            start_new_line( lines );
-            add_point_to_line( lines, &points[0] );
-            add_point_to_line( lines, &points[1] );
-        }
-    }
 }
