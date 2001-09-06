0036f470880c8a7078f1021c9174dcedb5d893f5
jason <jason>
2001-09-06 18:10:51 +0000

    surface_mask2 changed to force inclusion of object boundaries

--- surface_mask2.c
+++ surface_mask2.c
@@ -1,3 +1,16 @@
+/*
+ * Code adapted from David MacDonald's surface_mask2
+ * Basic logic:
+ *   1) Read input file and input object
+ *   2) Create a binary volume of all voxels inside object
+ *   3) Force inclusion of object boundaries (this is the key difference
+ *      between this version and the old David MacDonald version).
+ *   4) Keep all original voxels inside binary mask, set the rest to 0
+ *
+ * Author: Jason Lerch <jason@bic.mni.mcgill.ca>
+ * Last changed: Sep 2001.
+ */
+
 #include  <internal_volume_io.h>
 #include  <bicpl.h>
 
@@ -14,15 +27,16 @@ private  BOOLEAN  get_extrapolated_value(
     Real           threshold,
     Real           *value_to_set );
 
-int  main(
-    int   argc,
-    char  *argv[] )
+
+int binary_object_mask( STRING input_surface_filename,
+                        Real set_value,
+                        Real inside_value,
+                        Volume volume ) 
 {
-    STRING               input_volume_filename, input_surface_filename;
-    STRING               output_volume_filename;
-    Real                 set_value, separations[MAX_DIMENSIONS];
-    Real                 min_value, max_value, value;
-    Real                 xw, yw, zw, threshold, value_to_set;
+
+    Real                 separations[MAX_DIMENSIONS];
+    Real                 value;
+    Real                 xw, yw, zw, value_to_set;
     Real                 sign_before, sign_after;
     Point                origin, dest;
     Real                 min_real_value, max_real_value;
@@ -30,7 +44,6 @@ int  main(
     Vector               direction;
     STRING               history;
     File_formats         format;
-    Volume               volume;
     int                  x, y, z, n_objects, k;
     int                  sizes[MAX_DIMENSIONS];
     int                  obj_index;
@@ -41,28 +54,6 @@ int  main(
     BOOLEAN              threshold_specified;
     progress_struct      progress;
 
-    initialize_argument_processing( argc, argv );
-
-    if( !get_string_argument( NULL, &input_volume_filename ) ||
-        !get_string_argument( NULL, &input_surface_filename ) ||
-        !get_string_argument( NULL, &output_volume_filename ) )
-    {
-        print_error(
-            "Usage: %s  in_volume.mnc  in_surface.obj  out_volume.mnc\n",
-               argv[0] );
-        print_error( "      [min_value max_value] [set_value] [threshold]\n" );
-        return( 1 );
-    }
-
-    (void) get_real_argument( 0.0, &min_value );
-    (void) get_real_argument( min_value - 1.0, &max_value );
-    set_value_specified = get_real_argument( 0.0, &set_value );
-    threshold_specified = get_real_argument( 0.0, &threshold );
-
-    if( input_volume( input_volume_filename, 3, XYZ_dimension_names,
-                      NC_UNSPECIFIED, FALSE, 0.0, 0.0,
-                      TRUE, &volume, NULL ) != OK )
-        return( 1 );
 
     if( input_graphics_file( input_surface_filename,
                              &format, &n_objects, &objects ) != OK )
@@ -79,13 +70,6 @@ int  main(
 
     min_real_value = get_volume_real_min( volume );
     max_real_value = get_volume_real_max( volume );
-
-    if( !set_value_specified )
-        set_value = min_real_value;
-    else if( set_value < min_real_value )
-        set_value = min_real_value;
-    else if( set_value > max_real_value )
-        set_value = max_real_value;
 
     if( BINTREE_FACTOR > 0.0 )
     {
@@ -220,36 +204,23 @@ int  main(
                     erase_flag = FALSE;
                     if( !inside )
                     {
-                        if( min_value <= max_value )
-                        {
-                            value = get_volume_real_value( volume, x, y, z,0,0);
-                            erase_flag = min_value <= value &&
-                                         value <= max_value;
-                        }
-                        else
-                            erase_flag = TRUE;
+                      erase_flag = TRUE;
                     }
-
                     if( erase_flag )
                     {
-                        if( !threshold_specified ||
-                            !get_extrapolated_value( volume, x, y, z,
-                                        objects[0], threshold, &value_to_set ) )
-                        {
-                            value_to_set = set_value;
-                        }
-                        else if( value_to_set < min_real_value )
-                            value_to_set = min_real_value;
-                        else if( value_to_set > max_real_value )
-                            value_to_set = max_real_value;
-
-                        set_volume_real_value( volume, x, y, z, 0, 0,
-                                               value_to_set );
+                      value_to_set = set_value;
+                      set_volume_real_value( volume, x, y, z, 0, 0,
+                                             value_to_set );
+                    }
+                    else {
+                      set_volume_real_value( volume, x, y, z, 0, 0, 
+                                             inside_value );
                     }
                 }
             }
-            else
+            else {
                 print( "N intersections: %d\n", n_intersections );
+            }
 
             if( n_intersections > 0 )
             {
@@ -260,21 +231,13 @@ int  main(
             update_progress_report( &progress, x * sizes[Y] + y + 1 );
         }
     }
-
+    
     terminate_progress_report( &progress );
+
+    /* force intersection to be part of mask */
+    scan_object_to_volume(objects[0], volume, volume, inside_value, 1);
     
     delete_object_list( n_objects, objects );
-
-    history = create_string( "Surface masked.\n" );
-
-    (void) output_volume( output_volume_filename, NC_UNSPECIFIED,
-                            FALSE, 0.0, 0.0, volume, history,
-                            NULL );
-
-    delete_string( history );
-
-    delete_volume( volume );
-
     return( 0 );
 }
 
@@ -384,3 +347,74 @@ private  BOOLEAN  get_extrapolated_value(
 
     return( n_bounds > 0 );
 }
+
+int main ( int argc, char *argv[] )
+{
+  STRING         input_volume_filename, output_file_name;
+  STRING         surface_filename;
+  Real           outside_value, inside_value;
+  Volume         binary_volume, out_volume;
+  int            sizes[MAX_DIMENSIONS];
+  int            x,y,z;
+  Real           original_value, binary_value;
+  object_struct  **objects;  
+  File_formats   format;
+  int            n_objects;
+
+  outside_value = 0;
+  inside_value = 1;
+  
+  initialize_argument_processing( argc, argv );
+
+  if ( !get_string_argument( NULL, &input_volume_filename ) ||
+       !get_string_argument( NULL, &surface_filename ) ||
+       !get_string_argument( NULL, &output_file_name )
+       ) {
+    print_error("Usage: %s in_volume.mnc surface.obj output.mnc \n", argv[0] );
+    return( 1 );
+  }
+
+  /*
+   * create two copies of the volume - one to use for the binary mask,
+   * the other to store the original volume in.
+   */
+
+  if( input_volume( input_volume_filename, 3, XYZ_dimension_names,
+                    NC_BYTE, FALSE, 0.0, 0.0, TRUE, 
+                    &binary_volume, NULL ) != OK )
+    return( 1 );
+
+  out_volume = copy_volume(binary_volume);
+
+  /* create the binary mask */
+  (void) binary_object_mask( surface_filename,
+                             outside_value,
+                             inside_value,
+                             binary_volume );
+
+  get_volume_sizes( out_volume, sizes );
+
+  /* now test each voxel for inclusion */
+  for_less( x, 0, sizes[0] ) {
+    for_less( y, 0, sizes[1] ) {
+      for_less( z, 0, sizes[2] ) {
+        original_value = get_volume_real_value(out_volume, x, y, z, 0, 0);
+        binary_value = get_volume_real_value(binary_volume, x, y, z, 0, 0);
+        if (binary_value > 0.5) {
+          /* keep original value */
+          set_volume_real_value(out_volume, x, y, z, 0, 0, original_value);
+        }
+        else {
+          /* erase */
+          set_volume_real_value(out_volume, x, y, z, 0, 0, 0);
+        }
+      }
+    }
+  }
+
+  output_volume(output_file_name, NC_BYTE, FALSE, 0.0, 0.0,
+                out_volume, "", NULL );
+
+}
+
+
