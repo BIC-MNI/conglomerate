ac9c8e53bad1bcfaad6053350518a94995eba178
bert <bert>
2005-05-26 19:57:41 +0000

    Yasser's changes

--- multispectral_stx_registration.pl
+++ multispectral_stx_registration.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/env perl
+#!/usr/bin/env perl
 
 # stereotaxic registration for the VU Amsterdam protocol images
 
@@ -9,7 +9,10 @@ use Getopt::Tabular;
 
 # global variables
 my $model = undef;
+my $premodel= undef;
 my $modelDir = undef;
+my $premodelDir= undef;
+my $t1suppressed= undef;
 my $threshold = 1;
 
 # argument handling
@@ -18,9 +21,15 @@ my @argTbl =
   (
    @DefaultArgs,
    ["-model", "string", 1, \$model,
-    "set the base name of the fit model files"],
+    "set the base name of the main fit model files"],
+   ["-premodel", "string", 1, \$premodel,
+    "set the base name of the initial fit model files in two-stage registration"],
    ["-modeldir", "string", 1, \$modelDir,
-    "set the default directory to search for model files"],
+    "set the default directory to search for main fit model files"],
+   ["-premodeldir", "string", 1, \$premodelDir,
+    "set the default directory to search for initial fit model files in two-stage registration"],
+	["-t1suppressed", "string", 1, \$t1_suppressed,
+	 "set the suppressed file name in two-stage registration"],
    ["-threshold", "boolean", undef, \$threshold,
     "Enable thresholding in mritoself"],
   );
@@ -41,6 +50,11 @@ system("mkdir -p $TmpDir") unless (-d $TmpDir);
 RegisterPrograms([qw/mritotal
                   mritoself
                   xfmconcat/]);
+### in case of two-stage registration
+if ($premodel) {
+	RegisterPrograms(qw/mritotal_suppress)
+}
+
 
 if ($Clobber) {
   AddDefaultArgs("mritotal", ["-clobber"]);
@@ -51,12 +65,30 @@ if (! $threshold) {
     AddDefaultArgs("mritoself", ["-nothreshold"]);
 }
 
+if ($premodel) {
+	AddDefaultArgs("mritotal_suppress", ["-premodel"], ["-premodelDir"], ["-keepsuppressed"])
+}
+
 # step 1: T1 registration to talairach space
+### single-stage registration:
+unless ($premodel) {
 my @mritotalOptions;
 push @mritotalOptions, ["-modeldir", $modelDir] if $model;
 push @mritotalOptions, ["-model", $model] if $model;
 
 Spawn(["mritotal", @mritotalOptions, $nativeT1, $T1total]);
+}
+
+### two-stage registration:
+if ($premodel) {
+my @mritotal_suppressOptions;	
+push @mritotal_suppressOptions, ["-premodeldir", $premodelDir] if $premodel;
+push @mritotal_suppressOptions, ["-premodel", $premodel] if $premodel;
+push @mritotal_suppressOptions, ["-modeldir", $modelDir] if $model;
+push @mritotal_suppressOptions, ["-model", $model] if $model;
+
+Spawn(["mritotal_suppress", @mritotal_suppressOptions, $t1suppressed, $nativeT1, $T1total])
+}
 
 # step 2: registration of T2/PD to native T1
 my $T2toT1 = "${TmpDir}/t2_to_t1.xfm";
